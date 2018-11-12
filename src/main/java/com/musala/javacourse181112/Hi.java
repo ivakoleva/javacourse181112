cd68b44dffa40155321338e0127530974591f3c4
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-12 20:41:51 +0200

    Hi class

---
+++ src/main/java/com/musala/javacourse181112/Hi.java
@@ -0,0 +1,11 @@
+package com.musala.javacourse181112;
+
+/**
+ * Created by Iva Koleva, ClearCode Ltd. on 12.11.2018,
+ * a significant bit of leva.bg project.
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
