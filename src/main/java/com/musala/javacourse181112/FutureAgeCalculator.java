be48fb33ebd5dd22ce26a9993812d9984062d18b
VermilionPride <dimitar.ang.mgu@gmail.com>
2018-11-15 17:41:37 +0200

    Future age calculator string transformation

--- src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
@@ -2,11 +2,10 @@ package com.musala.javacourse181112;
 
 public class FutureAgeCalculator {
     public static void main(String[] args) {
-        int age = 25;
-        System.out.println("The age that you inputted is " + age);
-        int add = 6;
-        System.out.println("The age that you want to add is " + add);
-        add = add + age;
+        System.out.println("The age that you inputted is " + Integer.parseInt(args[0]));
+
+        System.out.println("The age that you want to add is " + Integer.parseInt(args[1]));
+        int add = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
         System.out.println("Your future age is " + add);
         System.exit(0);
     }
