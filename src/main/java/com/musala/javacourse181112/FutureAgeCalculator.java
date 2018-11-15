9466125f66392c6f9660529546fee076caf5fbfa
Blueshadows <blueshadows98@gmail.com>
2018-11-15 17:29:09 +0200

    Future Age Calculator

---
+++ src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
@@ -0,0 +1,14 @@
+package com.musala.javacourse181112;
+
+public class FutureAgeCalculator {
+    public static void main(String[] args) {
+        int age=25;
+        int addAge=5;
+
+        System.out.println("Age:"+age+"Added Age:"+addAge);
+        System.out.println("Result:"+(age+addAge));
+System.exit(0);
+
+    }
+
+}
