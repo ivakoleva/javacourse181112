931b8dfd966ab51ac0dcefad517c2b70e405a4db
boris <boris.dishev@gmail.com>
2018-11-15 18:37:45 +0200

    Age calculator

--- src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
@@ -5,7 +5,7 @@ public class FutureAgeCalculator {
     public static void main(String[] args) {
         System.out.println("Input: " + (args[0]));
         System.out.println("Input: " + args[1]);
-        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        System.out.println("Future age: " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
 
         System.exit(0);
     }
