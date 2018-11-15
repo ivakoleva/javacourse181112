d7e110c5e5d33319fbf8843164f455fa512ca5db
Blueshadows <blueshadows98@gmail.com>
2018-11-15 20:37:30 +0200

    Future Age Calculator

--- src/main/java/com/musala/javacourse181112/Voting.java
+++ src/main/java/com/musala/javacourse181112/Voting.java
@@ -4,13 +4,14 @@ public class Voting {
     public static void main(String[] args) {
         String [][] votes=new String [2][30];
         System.out.println("Question 1 Do You like snow");
-        votes[0][0]="yes";
-        votes[0][1]="no";
-        votes[0][2]="yes";
+
+        System.out.println(votes[0][0]="yes");
+        System.out.println(votes[0][1]="no");
+        System.out.println(votes[0][2]="yes");
 System.out.println("Question 2:Do you want it to snow today?");
-        votes[1][0]="yes";
-        votes[1][1]="no";
-        votes[1][2]="no";
+        System.out.println( votes[1][0]="yes");
+        System.out.println(votes[1][1]="no");
+        System.out.println(votes[1][2]="no");
 votesCounter(votes);
     }
    public static int votesCounter(String [][] vote){
