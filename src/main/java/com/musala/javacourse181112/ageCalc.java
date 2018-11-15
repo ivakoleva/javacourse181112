37df955e3cf5d760d48e029b3519d9cec0081c47
cecoweee <newcecoweee@gamil.com>
2018-11-15 18:11:03 +0200

    Age calc

--- src/main/java/com/musala/javacourse181112/ageCalc.java
+++ src/main/java/com/musala/javacourse181112/ageCalc.java
@@ -8,13 +8,16 @@ public class ageCalc {
 
 
     public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Въведете възраст: ");
-        int age = sc.nextInt();
-        System.out.println("Въведете изминали години: ");
-        int addAge = sc.nextInt();
-        int ageAfter = age + addAge;
-        System.out.println("Годините на човека са :" + ageAfter);
+        int age = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        System.out.println(age);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Въведете възраст: ");
+//        int age = sc.nextInt();
+//        System.out.println("Въведете изминали години: ");
+//        int addAge = sc.nextInt();
+//        int ageAfter = age + addAge;
+//        System.out.println("Годините на човека са :" + ageAfter);
 //        Integer.
 
     }
