2fedfe3f37bcda3854982918f15322007a0ef382
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-15 20:21:59 +0200

    code review

--- src/main/java/com/musala/javacourse181112/VoteProgram.java
+++ src/main/java/com/musala/javacourse181112/VoteProgram.java
@@ -1,21 +1,24 @@
 package com.musala.javacourse181112;
+
 import java.util.Random;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
+// nice
 public class VoteProgram {
-    public static void main(String[] args) {
-        String[] votes = new String[30];
-        Random rand = new Random();
+    private static final Random RANDOM = new Random();
+
+    public static void main(final String[] args) {
+        final String[] votes = new String[30];
+
+        // generate 30 votes
         for (int i = 0; i < 30; i++) {
-            int vote = rand.nextInt(2) + 1;
-            if (vote == 1) {
-                votes[i] = "Yes";
-                continue;
-            }
-            votes[i]="No";
+            votes[i] = RANDOM.nextInt(2) + 1 == 1 ?
+                    "Yes" : "No"; // expression ? if-true-do-that : else-do-that
         }
+
         Arrays.stream(votes)
                 .collect(Collectors.groupingBy(s -> s))
-                .forEach((k, v) -> System.out.println(k+" "+v.size()));
+                .forEach((k, v) -> System.out.println(k + " " + v.size()));
     }
 }
