416d1486b129bd05d3b41d7ecb6360fc09769a47
cecoweee <newcecoweee@gamil.com>
2018-11-15 18:16:34 +0200

    Age calc

---
+++ src/main/java/com/musala/javacourse181112/futureAgeCalc.java
@@ -0,0 +1,25 @@
+package com.musala.javacourse181112;
+
+
+import java.util.Scanner;
+
+
+public class futureAgeCalc {
+
+
+    public static void main(String[]args){
+        int age = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        System.out.println("Future age calculated: " + age);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Въведете възраст: ");
+//        int age = sc.nextInt();
+//        System.out.println("Въведете изминали години: ");
+//        int addAge = sc.nextInt();
+//        int ageAfter = age + addAge;
+//        System.out.println("Годините на човека са :" + ageAfter);
+//        Integer.
+
+    }
+
+}
