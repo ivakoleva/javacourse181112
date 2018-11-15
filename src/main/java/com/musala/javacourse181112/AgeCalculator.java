e0a8c30678014092394db79a1c6f076aba023584
Bzahov98 <bzahov1998@gmail.com>
2018-11-15 17:51:15 +0200

    [ADD] Age calculator

---
+++ src/main/java/com/musala/javacourse181112/AgeCalculator.java
@@ -0,0 +1,9 @@
+package com.musala.javacourse181112;
+
+public class AgeCalculator {
+    public static void main(String[] args) {
+        //System.out.println(args.length);
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        System.exit(0);
+    }
+}
