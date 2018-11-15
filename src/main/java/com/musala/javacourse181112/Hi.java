72ba7540a1c539cc8800aa71955aa61b016977a3
boris <boris.dishev@gmail.com>
2018-11-15 16:35:37 +0200

    branch

--- src/main/java/com/musala/javacourse181112/Hi.java
+++ src/main/java/com/musala/javacourse181112/Hi.java
@@ -1,5 +1,7 @@
 package com.musala.javacourse181112;
 
+//import java.util.Scanner;
+
 /**
  * Created by Iva Koleva, ClearCode Ltd. on 12.11.2018,
  * a significant bit of leva.bg project.
@@ -7,5 +9,15 @@ package com.musala.javacourse181112;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("Hi!");
+         System.gc(); //garbage collector
+        System.exit(0) ;
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = scanner.nextInt();
+        System.out.print("b = ");
+        int b = scanner.nextInt();
+        System.out.println("The sum is: " + (a + b));
+        */
+
     }
 }
