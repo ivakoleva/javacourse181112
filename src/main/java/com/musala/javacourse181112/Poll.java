65c06ef16f5b1a0234723cc1c391e8074aa6a642
dgluharov <dgluharov@gmail.com>
2018-11-15 20:15:56 +0200

    Poll

---
+++ src/main/java/com/musala/javacourse181112/Poll.java
@@ -0,0 +1,9 @@
+package com.musala.javacourse181112;
+
+public class Poll {
+    public static void  main(final String[] args) {
+        final String[] [] voters = new String[30][30:
+        System.out.println("Welcome! \nFirst question: \nIt is snowing today?");
+        ]
+    }
+}
