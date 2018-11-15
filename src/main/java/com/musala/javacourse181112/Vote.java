632272460246853d749046c3f2f0bed10e28a248
cecoweee <newcecoweee@gamil.com>
2018-11-15 20:02:23 +0200

    Vote

---
+++ src/main/java/com/musala/javacourse181112/Vote.java
@@ -0,0 +1,30 @@
+package com.musala.javacourse181112;
+
+public class Vote {
+    public static void main(String[] args) {
+        System.out.println("Wellcome ,you can vote now");
+        String[][] voteArr = new String[2][30];
+        String v1 = "yes";
+        String v2 = "no";
+        String v3 = "yes";
+        String v4 = "yes";
+        String v5 = "no";
+        String v6 = "yes";
+
+        voteArr[0][0] = v1;
+        voteArr[0][1] = v2;
+        voteArr[0][2] = v3;
+        voteArr[1][0] = v4;
+        voteArr[1][1] = v5;
+        voteArr[1][2] = v6;
+        System.out.println("Отговори на първи въпрос: ");
+        for (int i = 0; i < voteArr.length + 1; i++) {
+            System.out.println(voteArr[0][i]);
+        }
+        System.out.println("Отговори на втори въпрос");
+        for (int i = 0; i <voteArr.length +1; i++) {
+            System.out.println(voteArr[1][i]);
+
+        }
+    }
+}
--- src/main/java/com/musala/javacourse181112/basics/StringExample.java
+++ src/main/java/com/musala/javacourse181112/basics/StringExample.java
@@ -1,15 +1,12 @@
 package com.musala.javacourse181112.basics;
 
-/**
- * Created by Iva Koleva on 15.11.2018
- */
+import java.sql.SQLOutput;
+
 public class StringExample {
     public static void main(final String[] args) {
         String value = "asd";
+        System.out.println(value + "asd d1    1  ");
         String valueUsingConstructor = new String("asd");
-        System.out.println(valueUsingConstructor.equals(value));
 
-        System.out.println(value + " asd1");
-        System.out.println(value.concat(" asd1"));
     }
 }
