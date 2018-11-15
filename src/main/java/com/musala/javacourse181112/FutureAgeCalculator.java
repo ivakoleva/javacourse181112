2909a4e3a29059223da36e44a787fdb6d7ec6d99
Antonio <antoniobuyukliev@yahoo.com>
2018-11-15 17:26:03 +0200

    FutureAgeCalculator

---
+++ src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
@@ -0,0 +1,9 @@
+package com.musala.javacourse181112;
+
+public class FutureAgeCalculator {
+    public static void main(String[] args) {
+        System.out.println("Input age: " + args[0]);
+        System.out.println("Input period: " + args[1]);
+        System.out.println("Age in future: " + (args[0] + args[1]));
+    }
+}
