062fb4f3fe66073a20f61c72f76fb7284eed5c48
Blueshadows <blueshadows98@gmail.com>
2018-11-15 18:33:31 +0200

    Future Age Calculator

--- src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
@@ -2,10 +2,8 @@ package com.musala.javacourse181112;
 
 public class FutureAgeCalculator {
     public static void main(String[] args) {
-        int age=0;
-        int addYears=5;
-
-        age=Integer.parseInt(args[0]);
+        int age=Integer.parseInt(args[0]);
+        int addYears=Integer.parseInt(args[1]);
 
         System.out.println("Age:"+age+"\nAdded Age:"+addYears);
         System.out.println("Result:"+(age+addYears));
