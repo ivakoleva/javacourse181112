4760cb9291a977931dd901f01a7e4cb81145129f
VermilionPride <dimitar.ang.mgu@gmail.com>
2018-11-15 20:01:09 +0200

    Future age calculator string transformation

---
+++ src/main/java/com/musala/javacourse181112/Poll.java
@@ -0,0 +1,21 @@
+package com.musala.javacourse181112;
+
+public class Poll {
+    public static void main(String[] args) {
+        String[][] voters = new String[30][30];
+        System.out.println("Welcome!!! \nThe first question is: \nDo you like pizza?");
+        for(int i=0; i<30; i++){
+            if(i/2==0) voters[i][0]="Yes";
+                    else voters[i][0]="No";
+
+        }
+
+        for(int i=0; i<30; i++){
+            if(i/2==0) voters[i][1]="No";
+            else voters[i][1]="Yes";
+
+        }
+        System.exit(0);
+    }
+
+}
