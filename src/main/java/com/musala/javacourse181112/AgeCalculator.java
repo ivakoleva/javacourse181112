a7148dc21e2e5dfaf669d21e9e639074016d2bd0
anton kolev <antongtkolev@gmail.com>
2018-11-15 18:18:16 +0200

    initial commit

---
+++ src/main/java/com/musala/javacourse181112/AgeCalculator.java
@@ -0,0 +1,13 @@
+package com.musala.javacourse181112;
+
+public class AgeCalculator {
+    public static void main(String[] args){
+         System.out.println(args[0] + " " + args[1]);
+
+         int age  = Integer.parseInt(args[0]);
+         int yearToAdd = Integer.parseInt(args[1]);
+         System.out.println("Future age calculated: " + (age + yearToAdd));
+
+         System.exit(0);
+    }
+}
