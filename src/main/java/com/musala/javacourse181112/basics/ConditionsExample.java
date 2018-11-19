22602adf631ab14fed403012eb05980a07fa5c6b
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-19 18:27:52 +0200

    ternary implemented; switch example

--- src/main/java/com/musala/javacourse181112/basics/ConditionsExample.java
+++ src/main/java/com/musala/javacourse181112/basics/ConditionsExample.java
@@ -40,5 +40,49 @@ public class ConditionsExample {
 
         // ternary operator ?:
         final String variable = 1 != 1 ? "true" : "false";
+
+        // switch
+        String value = "yes";
+        if ("yes".equals(value)) {
+            System.out.println("value: yes");
+        } else if ("no".equals(value)) {
+            System.out.println("value: no");
+        } else if ("asd".equals(value)) {
+            System.out.println("value: asd");
+        } else if ("asd1".equals(value)) {
+            System.out.println("value: asd1");
+        }
+
+        switch (value) {
+            case "yes":
+                //
+                System.out.println("value: yes");
+                //break;
+            case "no":
+                System.out.println("value: no");
+                //break;
+            case "asd":
+                System.out.println("value: asd");
+                //break;
+            case "asd1":
+                System.out.println("value: asd1");
+                //break;
+            default:
+                System.out.println("default");
+                break;
+        }
+
+        switch (value) {
+            case "prof":
+                //
+                System.out.println("value: yes");
+                //break;
+            case "dr":
+                System.out.println("value: no");
+                //break;
+            case "a":
+                System.out.println("value: asd");
+                //break;
+        }
     }
 }
