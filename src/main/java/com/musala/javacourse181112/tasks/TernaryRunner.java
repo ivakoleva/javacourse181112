723c7916f8ef884cfe57c5ebe379a5623954a994
stefan.atanasov <stefanatanasov99@abv.bg>
2018-11-19 18:27:59 +0200

    Merge remote-tracking branch 'origin/master' into samplerunner
    # Conflicts:
    #	src/main/java/com/musala/javacourse181112/Hi.java

--- src/main/java/com/musala/javacourse181112/tasks/TernaryRunner.java
+++ src/main/java/com/musala/javacourse181112/tasks/TernaryRunner.java
@@ -11,14 +11,41 @@ public class TernaryRunner {
         int noCounter = 0;
 
         if("yes".equals(value)) {
+
             yesCounter++;
         } else if("no".equals(value)) {
             noCounter++;
         }
         // TODO: 2-lines implementation, using ternary operator
+
+
+        final int checkYes="yes".equals(value)?yesCounter++:noCounter++;
+        final int checkNo="no".equals(value);
+
+
+                //switch
+
+        switch (value) {
+            case "prof" :
+                //
+                System.out.println("value:yes");
+                break;
+            case "dr" :
+                System.out.println("value: yes ");
+                break;
+            case "a":
+                System.out.println("value :asd");
+                break;
+            case "asd1":
+                System.out.println("value = asd1");
+                break
+            case "asd2"
+             System.out.println("defalut");
+             break;
+             default:
+                 System.out.println();
+        }
     }
-
-
 }
 
 
