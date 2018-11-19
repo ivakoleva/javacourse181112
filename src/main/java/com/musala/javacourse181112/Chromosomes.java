03e5374ec4a5d6b229e03e9defed1df14209c5be
muzun97 <muzun@abv.bg>
2018-11-19 18:48:35 +0200

    gender checker

---
+++ src/main/java/com/musala/javacourse181112/Chromosomes.java
@@ -0,0 +1,14 @@
+package com.musala.javacourse181112;
+
+public class Chromosomes {
+    public static void main(String[] args) {
+        switch(args[0]) {
+            case "male":
+                System.out.println("yx");
+                break;
+            case "female":
+                System.out.println("xx");
+                break;
+        }
+    }
+}
