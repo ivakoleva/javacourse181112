2454e5b2cd9336348e80a48287464e3dd42cc355
Blueshadows <blueshadows98@gmail.com>
2018-11-19 18:13:22 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/TernaryRunner.java
+++ src/main/java/com/musala/javacourse181112/tasks/TernaryRunner.java
@@ -7,15 +7,16 @@ public class TernaryRunner {
     public static void main(final String[] args) {
         String value = "yes";
 int  a=0;
+
         int yesCounter = 0;
         int noCounter = 0;
 
-     /*   if("yes".equals(value)) {
+     /  if("yes".equals(value)) {
             yesCounter++;
         } else if("no".equals(value)) {
             noCounter++;
-        }*/
-a=("yes".equals(value)? yesCounter++:noCounter++);
+        }
+    a="yes".equals(value)? yesCounter++:noCounter++;
         System.out.println("Yes: "+ yesCounter+"\nNo: "+noCounter);
 
         // TODO: 2-lines implementation, using ternary operator
