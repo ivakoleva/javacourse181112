e4cc03a9d236d709a94aeeeeef32d99c7b053627
anton kolev <antongtkolev@gmail.com>
2018-11-19 18:51:03 +0200

    Conditions

---
+++ src/main/java/com/musala/javacourse181112/basics/ChromosomesApp.java
@@ -0,0 +1,19 @@
+package com.musala.javacourse181112.basics;
+
+public class ChromosomesApp {
+    public static void main(String[] args) {
+        String male ="male";
+        String famele = "female";
+        String maleChromo="xy";
+        String femaleChromo = "xx";
+
+        System.out.println("What is your gender :   ? ");
+
+       switch (maleChromo){
+           case "yx" :
+               System.out.println(male);
+               break;
+           case "xx"
+       }
+    }
+}
