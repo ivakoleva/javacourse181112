5ef04a2b0583f222407112e26f770f9ebfb15a49
Blueshadows <blueshadows98@gmail.com>
2018-11-19 20:32:25 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/Voting.java
+++ src/main/java/com/musala/javacourse181112/tasks/Voting.java
@@ -6,19 +6,19 @@ public class Voting {
     public static final int VOTES_COUNT = 30;
     public static final String QUESTION_PREFIX = "Question ";
     public static final String QUESTION_SEPARATOR = ": ";
-   public static final String YES = "yes";
-    public static final String NO = "no";
+//   public static final String YES = "yes";
+  //  public static final String NO = "no";
 
     public static void main(final String[] args) {
-        final String[][] votes = new String[QUESTIONS_COUNT][VOTES_COUNT];
+        final Vote[][] votes = new Vote[QUESTIONS_COUNT][VOTES_COUNT];
 
         int qIndex = 0; // TODO: use loops
 
         // populate data
         final String q0 = "Do You like snow?";
-        votes[qIndex][0] = YES;
-        votes[qIndex][1] = NO;
-        votes[qIndex][2] = YES;
+        votes[qIndex][0] = Vote.NO;
+        votes[qIndex][1] = Vote.YES;
+        votes[qIndex][2] = Vote.NO;
 
         // print data
         printQuestion(qIndex, q0);
@@ -26,9 +26,9 @@ public class Voting {
 
         // populate data
         final String q1 = "Do you want it to snow today?";
-        votes[++qIndex][0] = YES;
-        votes[qIndex][1] = NO;
-        votes[qIndex][2] = NO;
+        votes[++qIndex][0] = Vote.YES;
+        votes[qIndex][1] = Vote.NO;
+        votes[qIndex][2] = Vote.YES;
 
         // print data
         printQuestion(qIndex, q1);
@@ -41,7 +41,7 @@ public class Voting {
         System.out.println(QUESTION_PREFIX + (index + 1) + QUESTION_SEPARATOR + content);
     }
 
-    public static void printVotes(final String[][] votes, final int questionIndex) {
+    public static void printVotes(final Vote[][] votes, final int questionIndex) {
         // TODO: use loops
         System.out.println("Vote 1: " + votes[questionIndex][0]);
         System.out.println("Vote 2: " + votes[questionIndex][1]);
@@ -49,7 +49,19 @@ public class Voting {
     }
 
     // TODO: implement
-    public static int countVotes(final String[][] votes) {
-        return 0;
+    public static void countVotes(final Vote[][] votes) {
+        int voteYes=0;
+        int voteNo=0;
+        for(int i=0;i<votes.length;i++){
+            for(int j=0;j<votes[i].length;j++){
+                if(votes[i][j]==Vote.YES){
+                    voteYes++;
+                }else{
+                    voteNo++;
+                }
+          //      System.out.println("Votes for Question"+votes[i+1]);
+            }
+        }
+
     }
 }
--- src/main/java/com/musala/javacourse181112/tasks/Voting_v_2.java
+++
@@ -1,8 +0,0 @@
-package com.musala.javacourse181112.tasks;
-
-public class Voting_v_2 {
-    public static void main(final String[] args) {
-
-
-    }
-}
