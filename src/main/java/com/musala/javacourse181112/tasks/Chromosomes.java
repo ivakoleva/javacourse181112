9c2a05eef48bd56b6316ec383c34b979d9241b4c
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-19 19:14:21 +0200

    code review

--- src/main/java/com/musala/javacourse181112/tasks/Chromosomes.java
+++ src/main/java/com/musala/javacourse181112/tasks/Chromosomes.java
@@ -3,20 +3,20 @@ package com.musala.javacourse181112.tasks;
 import java.util.Scanner;
 
 public class Chromosomes {
-    private static final Scanner CONSOLE= new Scanner(System.in);
-    private static void chromosomeForGender(Scanner CONSOLE){
+    private static final Scanner SCANNER = new Scanner(System.in); // could be a local variable
+
+    private static void chromosomeForGender() {
         System.out.println("Chose gender :");
-        switch(CONSOLE.next()) {
-            case"female":
+        switch (SCANNER.next()) {
+            case "female":
                 System.out.println("The Female chromosome is:XX ");
                 break;
             case "male":
                 System.out.println("The Male chromosome is:XY ");
         }
     }
-public static void main(String[]args) {
 
-
-    chromosomeForGender(CONSOLE);
- }
+    public static void main(final String[] args) {
+        chromosomeForGender();
+    }
 }
