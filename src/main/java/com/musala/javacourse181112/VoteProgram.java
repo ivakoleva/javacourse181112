6ea2708ed7cc172a9d7f8a35491780a92d90a519
Antonio <antoniobuyukliev@yahoo.com>
2018-11-19 16:41:18 +0200

    VoteProgramUpgrade

--- src/main/java/com/musala/javacourse181112/VoteProgram.java
+++ src/main/java/com/musala/javacourse181112/VoteProgram.java
@@ -1,24 +1,33 @@
 package com.musala.javacourse181112;
 
-import java.util.Random;
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collectors;
 
 // nice
 public class VoteProgram {
     private static final Random RANDOM = new Random();
-
+    private static final int voters = 30;
     public static void main(final String[] args) {
-        final String[] votes = new String[30];
+
+        final Map<Integer, String[]> voting = new HashMap<>();
 
         // generate 30 votes
-        for (int i = 0; i < 30; i++) {
-            votes[i] = RANDOM.nextInt(2) + 1 == 1 ?
-                    "Yes" : "No"; // expression ? if-true-do-that : else-do-that
+        for (int i = 1; i <=5 ; i++) {
+            final String[] votes = new String[voters];
+            for (int j = 0; j < voters; j++) {
+                votes[j] = RANDOM.nextInt(2) + 1 == 1 ? "Yes" : "No"; // expression ? if-true-do-that : else-do-that
+            }
+            voting.put(i, votes);
         }
 
-        Arrays.stream(votes)
-                .collect(Collectors.groupingBy(s -> s))
-                .forEach((k, v) -> System.out.println(k + " " + v.size()));
+        for (Map.Entry<Integer, String[]> entry:voting.entrySet()) {
+            System.out.println("Question: " + entry.getKey());
+            Arrays.stream(entry.getValue())
+                    .collect(Collectors.groupingBy(s -> s))
+                    .forEach((k, v) -> System.out.println(k+" "+v.size()));
+        }
+
+
+
     }
 }
