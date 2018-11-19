6f79e314d9594e5b19769c0a3aa8e9e1b3d4ed35
Blueshadows <blueshadows98@gmail.com>
2018-11-19 19:25:53 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/Chromosomes.java
+++ src/main/java/com/musala/javacourse181112/tasks/Chromosomes.java
@@ -3,22 +3,41 @@ package com.musala.javacourse181112.tasks;
 import java.util.Scanner;
 
 public class Chromosomes {
+public static final String MALE_CHROMOSOMES="XY";
+    public static final String FEMALE_CHROMOSOMES="XX";
+    public static void main( final String[] args) {
 
-    public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         String gender;
         System.out.println("Enter gender male or female");
         gender=input.nextLine();
-        switch(gender){
+      /*  switch(gender.toLowerCase()){
             case "male":
-                System.out.println("y");
+                System.out.println(MALE_CHROMOSOMES);
                 break;
             case "female":
-                System.out.println("The chromosomes for this gender are xx");
+                System.out.println(FEMALE_CHROMOSOMES);
                 break;
                 default:
                     System.out.println("Error");
                     break;
+
+
+        }
+        */
+
+        switch(gender){
+            case "male":
+                System.out.println("Y");
+
+            case "female":
+                System.out.println("X");
+                if("male".equalsIgnoreCase(gender))
+                    break;
+
+            default:
+                System.out.println("X");
+
 
 
         }
