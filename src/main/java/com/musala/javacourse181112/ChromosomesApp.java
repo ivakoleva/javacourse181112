e0bf8a912729fde30404cc3cfcbc4073d08a2206
anton kolev <antongtkolev@gmail.com>
2018-11-19 19:21:57 +0200

    Chromosoms

---
+++ src/main/java/com/musala/javacourse181112/ChromosomesApp.java
@@ -0,0 +1,22 @@
+package com.musala.javacourse181112;
+
+public class ChromosomesApp {
+    public static void main(String[] args) {
+        final String gender = args [0];
+
+        String chromosomesString = "";
+        switch (gender.toLowerCase()){
+            case "male" :
+                chromosomesString += "y";
+            case "female" :
+                chromosomesString += "x";
+                if (chromosomesString.length() == 2){
+                    break;
+                }
+            default:
+                chromosomesString += "x";
+        }
+        System.out.println("Chromosomes for gender : " + gender);
+        System.out.println(chromosomesString);
+    }
+}
