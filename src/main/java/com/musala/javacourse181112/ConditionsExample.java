061fcb101d3ff34c2451d4b39e0bc9079de1fc21
dgluharov <dgluharov@gmail.com>
2018-11-19 17:38:41 +0200

    Poll

---
+++ src/main/java/com/musala/javacourse181112/ConditionsExample.java
@@ -0,0 +1,8 @@
+package com.musala.javacourse181112;
+
+public class ConditionsExample {
+    public static void main(String[] args) {
+        if (4 > 5);
+            System.out.println("if");
+    }
+}
