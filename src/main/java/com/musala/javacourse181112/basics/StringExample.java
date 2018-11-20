34e27e1bf234b2acc155977f00af6bb646e77137
Niki <lambrev.niki@gmail.com>
2018-11-20 19:59:54 +0200

    string example

--- src/main/java/com/musala/javacourse181112/basics/StringExample.java
+++ src/main/java/com/musala/javacourse181112/basics/StringExample.java
@@ -1,5 +1,8 @@
 package com.musala.javacourse181112.basics;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Iva Koleva on 15.11.2018
  */
@@ -11,5 +14,19 @@ public class StringExample {
 
         System.out.println(value + " asd1");
         System.out.println(value.concat(" asd1"));
+
+        String value1="123";
+        String value2="12asd3";
+
+        final Pattern pattern = Pattern.compile(".*\\d+.*");
+        final Matcher  matcher1 = pattern.matcher(value2);
+        if (matcher1.matches());{
+            System.out.println("Value1 is valid! ["+ value1 +"]");
+        }
+        final Matcher matcher2 = pattern.matcher(value2);
+        boolean print = false;
+        while(matcher2.find()){
+            if(!print)
+        }
     }
 }
