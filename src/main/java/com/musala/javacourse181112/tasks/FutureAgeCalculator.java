433ab170beca1914ed446d3eec05c63ef50f5192
Vessy <veselabozheva@gmail.com>
2018-11-20 20:11:03 +0200

    future age calculator updated

--- src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
@@ -1,17 +1,46 @@
 package com.musala.javacourse181112.tasks;
+import  java.util.*;
 
 public class FutureAgeCalculator {
     public static void main(final String[] args) {
-        System.out.println("Input parameters: " + args[0] + " " + args[1]);
+        /*Scanner scanner = new Scanner(System.in);
+        int exitProgram;*/
+         do{
+             /*System.out.println("Type 1 if you want to exit, type any other key and enter to continue");
+             exitProgram = scanner.nextInt();*/
+             inputYears();
 
-        final int age = Integer.parseInt(args[0]);
-        final int yearToAdd = Integer.parseInt(args[1]);
+        }while(true);
 
-        System.out.println("Future age calculated: " + (age + yearToAdd));
-
-        /*System.out.println("Future age calculated: " +
-                (Integer.parseInt(args[0]) +
-                        Integer.parseInt(args[1])));*/
-        System.exit(0);
     }
+    public static void inputYears(){
+        System.out.println("Enter size of ages array ");
+        Scanner scanner = new Scanner(System.in);
+        int quantityOfAges = scanner.nextInt();
+        System.out.println("Enter size of added years array ");
+        int quantityOfYears = scanner.nextInt();
+
+
+        final int[] ages = new int[quantityOfAges];
+        final int[] yearsToAdd = new int[quantityOfYears];
+        int sum = 0;
+
+        System.out.println("Please enter ages ");
+        for (int age: ages)
+        {
+            age += scanner.nextInt();
+            sum += age;
+        }
+        System.out.println("Please enter years to add sum with the ages ");
+        for (int year: yearsToAdd)
+        {
+            year += scanner.nextInt();
+            sum += year;
+        }
+
+        System.out.println(sum);
+       }
+
+
+
 }
