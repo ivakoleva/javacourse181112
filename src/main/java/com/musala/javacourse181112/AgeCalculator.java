42f9e51bce75e9c88171126f9a701285390838c6
anton kolev <antongtkolev@gmail.com>
2018-11-20 19:25:35 +0200

    FutureAgeCalc V.2 with (do while)

--- src/main/java/com/musala/javacourse181112/AgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/AgeCalculator.java
@@ -1,18 +1,19 @@
-package com.musala.javacourse181112.tasks;
+package com.musala.javacourse181112;
 
-public class FutureAgeCalculator {
-    public static void main(final String[] args) {
+public class AgeCalculator {
+    public static void main(final String[] args){
         System.out.println("Input parameters: " + args[0] + " " + args[1]);
 
         final int age = Integer.parseInt(args[0]);
         final int yearToAdd = Integer.parseInt(args[1]);
+        int i = 0;
         int a = 0;
 
         System.out.println("Future age calculated: " + (age + yearToAdd));
         do {
-            a++;
-        } while (a<args.length);
-        System.out.println("The age is"+ (a));
+            a += Integer.parseInt(args[i++]);
+        } while (i<args.length);
+        System.out.println("The age is " + (a));
 
 
         /*System.out.println("Future age calculated: " +
