362bcfa480d467e20062ddf34a4444220819e6ce
Blueshadows <blueshadows98@gmail.com>
2018-11-20 19:28:36 +0200

    Age calculator+bonus

--- src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
@@ -8,29 +8,27 @@ public class FutureAgeCalculator {
 
         int age=0;
         int addYears=0;
+
 System.out.println("Enter your age: ");
 age=enterAge();
+
 do{
     System.out.println("Enter years you want to add to your age(Enter 0 to stop the app): ");
     addYears=enterAge();
     age+=addYears;
-
 }
 while(addYears!=0);
 System.out.println("Your future age is "+age);
-
 System.exit(0);
-
     }
+
     private static int enterAge() {
         String in = null;
         in = INPUT.next();
         if (in.contains(".")) {
             System.exit(1);
-
         }
             return Integer.parseInt(in);
-
     }
 
 }
