ab28d7fddbcd967a96fee1deaec713c9f6ebc36c
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-20 19:43:02 +0200

    code review

--- src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
@@ -6,40 +6,46 @@ public class FutureAgeCalculator {
         v2(args);
         System.exit(0);
     }
-    public static void v1(final String[] args){
-        int i=0;
-        while((i+1)<args.length) {
-            if(!Integer.toString(Integer.parseInt(args[i])).equals(args[i])||!Integer.toString(Integer.parseInt(args[i+1])).equals(args[i+1]))
-            {
-                System.out.println(1);
-                //System.exit(1);
+
+    public static void v1(final String[] args) {
+        int i = 0;
+        while ((i + 1) < args.length) {
+            // validate
+            // TODO: handle exception
+
+            final int age = Integer.parseInt(args[i]);
+            final int addYears = Integer.parseInt(args[i + 1]);
+
+            if (!args[i].equals(Integer.toString(age)) ||
+                    !Integer.toString(addYears).equals(args[i + 1])) {
+                //System.out.println(1);
+                System.exit(1);
             }
-            parser(args,i);
-            i+=2;
+            calculate(age, addYears);
+            i += 2;
         }
     }
-    public static void v2(final String[] args)
-    {
-        int k=0;
-        while(k<args.length){
-            for(int i=0;i<args[k].length();i++){
-                if(args[k].charAt(i)<'0'||args[k].charAt(i)>'9')
-                {
+
+    public static void v2(final String[] args) {
+        int k = 0;
+        while (k < args.length) {
+            for (int i = 0; i < args[k].length(); i++) {
+                // TODO: regex ?
+                // unicode table, for reference
+                if (args[k].charAt(i) < '0' || args[k].charAt(i) > '9') {
                     System.out.println(1);
                     //System.exit(1);
                 }
             }
-            if(k%2!=0){
-                parser(args,k-1);
+            if (k % 2 != 0) {
+                calculate(Integer.parseInt(args[k - 1]), Integer.parseInt(args[k]));
             }
-            k+=1;
+            k++;
         }
     }
-    public static void parser(final String[] args,int i){
-        int age=Integer.parseInt(args[i]);
-        int addYears = Integer.parseInt(args[i+1]);
 
-        System.out.println("Age:" + age + "\nAdded Age:" + addYears);
-        System.out.println("Result:" + (age + addYears));
+    public static void calculate(final int n1, final int n2) {
+        System.out.println("Age:" + n1 + "\nAdded Age:" + n2);
+        System.out.println("Result:" + (n1 + n2));
     }
 }
