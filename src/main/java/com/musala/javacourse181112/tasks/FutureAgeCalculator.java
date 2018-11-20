f27458f21819c666b5655b8c722b3954834c4c58
Blueshadows <blueshadows98@gmail.com>
2018-11-20 19:22:30 +0200

    Age calculator+bonus

--- src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
@@ -3,15 +3,16 @@ package com.musala.javacourse181112.tasks;
 import java.util.Scanner;
 
 public class FutureAgeCalculator {
+   public static Scanner INPUT=new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+
         int age=0;
         int addYears=0;
 System.out.println("Enter your age: ");
-age=Integer.parseInt(input.next());
+age=enterAge();
 do{
     System.out.println("Enter years you want to add to your age(Enter 0 to stop the app): ");
-    addYears=Integer.parseInt(input.next());
+    addYears=enterAge();
     age+=addYears;
 
 }
@@ -21,10 +22,15 @@ System.out.println("Your future age is "+age);
 System.exit(0);
 
     }
-    private static String enterAge(){
-        String in=null;
+    private static int enterAge() {
+        String in = null;
+        in = INPUT.next();
+        if (in.contains(".")) {
+            System.exit(1);
 
-        return in;
+        }
+            return Integer.parseInt(in);
+
     }
 
 }
