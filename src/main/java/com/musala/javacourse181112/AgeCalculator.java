fe5623b8324daf92de8192587fde6e1b9c2c99c1
muzun97 <muzun@abv.bg>
2018-11-20 18:57:04 +0200

    age calculator with while

--- src/main/java/com/musala/javacourse181112/AgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/AgeCalculator.java
@@ -2,18 +2,17 @@ package com.musala.javacourse181112;
 
 public class AgeCalculator {
     public static void main(String[] args) {
-        int age, ageAdded, ageAfter;
-        age = Integer.parseInt(args[0]);
-        ageAdded = Integer.parseInt(args[1]);
-        System.out.println("Your age:" + age);
-        System.out.println("Age to add:" + ageAdded);
-        ageAfter = age + ageAdded;
-        System.out.println("Your age after " + ageAdded + " years will be " + ageAfter );
+        int ageAfter = 0;
+       int i = args.length;
+       i--;
 
+       do{
+          ageAfter += Integer.parseInt(args[i]);
+          i--;
+      }while(i != -1);
+
+        System.out.println(ageAfter);
         System.exit( 0);
-
-
-
 
     }
 }
