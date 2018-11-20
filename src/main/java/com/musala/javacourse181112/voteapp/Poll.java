8e67de9419846c8355fa98adfb558a7e4788f360
VermilionPride <dimitar.ang.mgu@gmail.com>
2018-11-20 16:36:01 +0200

    Optimised code

--- src/main/java/com/musala/javacourse181112/voteapp/Poll.java
+++ src/main/java/com/musala/javacourse181112/voteapp/Poll.java
@@ -2,13 +2,16 @@ package com.musala.javacourse181112.voteapp;
 
 // :)
 public class Poll {
+    static int votesYes = 0, votesNo = 0, i =0;
     public static void main(final String[] args) {
         final int maxVoters = 30;
         final int maxQuestions = 2;
 
         final String[][] voters = new String[maxVoters][maxQuestions];
 
-        System.out.println("Welcome!!! \nThe first question is: \nDo you like pizza?");
+        System.out.println("Welcome!!!");
+
+        PrintIn(1, "Do you like pizza?");
 
         // populate data
         for (int j = 0; j < maxQuestions; j++) {
@@ -16,31 +19,30 @@ public class Poll {
                 voters[i][j] = String.valueOf((i % 2) == 0 ? Vote.YES : Vote.NO);
             }
         }
+        VoteCounter(maxVoters, voters,0);
 
         // count votes
-        int votesYes = 0, votesNo = 0;
-        for (int i = 0; i < maxVoters; i++) {
-            if (String.valueOf(Vote.YES).equals(voters[i][0])) {
-                votesYes++;
-            } else {
-                votesNo++;
-            }
-        }
-        System.out.println(maxVoters + " voted out of which " + votesYes + " voted " + Vote.YES + " \nThe remaining " + votesNo + " voted " + Vote.NO + ".");
 
-        // TODO: method encapsulation of common code
-        System.out.println("\nThe second question is: \nIs it snowing?");
-        votesYes = votesNo = 0;
-        for (int i = 0; i < maxVoters; i++) {
-            if (voters[i][1].equals(String.valueOf(Vote.YES))) {
-                votesYes++;
-            } else {
-                votesNo++;
-            }
-        }
+
+
+
+
+        PrintIn(2, "Do you like snow?");
+        VoteCounter(maxVoters, voters,1);
         System.out.println(maxVoters + " voted out of which " + votesYes + " voted Yes \nThe remaining " + votesNo + " voted NO.");
-
-        System.exit(0);
     }
+    private static void PrintIn(int questionNumber, String questionContent){
+        System.out.println("\nQuestion number: " + questionNumber + "  "+ questionContent);
+    }
+    private static void VoteCounter(int maxVoters, String[][] voters, int m){
+        for (i = 0; i < maxVoters; i++) {
+            if (String.valueOf(Vote.YES).equals(voters[i][m])) {
+                votesYes++;
+            } else {
+                votesNo++;
+            }
+        }
+     System.out.println(maxVoters + " voted out of which " + votesYes + " voted " + Vote.YES + " \nThe remaining " + votesNo + " voted " + Vote.NO + ".");
+     }
 }
 
