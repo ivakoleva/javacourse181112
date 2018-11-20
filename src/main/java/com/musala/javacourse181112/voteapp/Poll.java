2df83bf003f7bee3012d43f03b71f5efbdef2647
VermilionPride <dimitar.ang.mgu@gmail.com>
2018-11-20 17:58:13 +0200

    Optimised code

--- src/main/java/com/musala/javacourse181112/voteapp/Poll.java
+++ src/main/java/com/musala/javacourse181112/voteapp/Poll.java
@@ -2,27 +2,20 @@ package com.musala.javacourse181112.voteapp;
 
 // :)
 public class Poll {
-    private static int votesYes, votesNo, i;
-    public static void main(final String[] args) {
-        final int maxVoters = 30;
-        final int maxQuestions = 2;
+    private static int votesYes, votesNo, i,j;
+    private static final int maxVoters = 30;
+    private static final int voterQualities = 3;
+    private static final int voteAttribute = 2;
 
-        final String[][] voters = new String[maxVoters][maxQuestions];
+    private static final Object[][] voters =new Object[maxVoters][voterQualities];
+    public static void main(final String[] args) {
 
         System.out.println("Welcome!!!");
-
+        populateVoters(voters);
         PrintIn(1, "Do you like pizza?");
-
-        // populate data
-        for (int j = 0; j < maxQuestions; j++) {
-            for (int i = 0; i < maxVoters; i++) {
-                voters[i][j] = String.valueOf((i % 2) == 0 ? Vote.YES : Vote.NO);
-            }
-        }
-
-        VoteCounter(maxVoters, voters,0);
+        VoteCounter(maxVoters, voters,voteAttribute);
         PrintIn(2, "Do you like snow?");
-        VoteCounter(maxVoters, voters,1);
+        VoteCounter(maxVoters, voters,voteAttribute);
 
     }
 
@@ -32,7 +25,7 @@ public class Poll {
     }
 
     //Vote counter and printer
-    private static void VoteCounter(int maxVoters, String[][] voters, int m){
+    private static void VoteCounter(int maxVoters, Object[][] voters, int m){
         votesYes = votesNo = 0;
         for (i = 0; i < maxVoters; i++) {
             if (String.valueOf(Vote.YES).equals(voters[i][m])) {
@@ -42,6 +35,23 @@ public class Poll {
             }
         }
     System.out.println(maxVoters + " voted out of which " + votesYes + " voted " + Vote.YES + " \nThe remaining " + votesNo + " voted " + Vote.NO + ".");
+    }
+    
+    //Voter populator
+    private static void populateVoters(Object[][] voters){
+        long EGN = 8902034520L;
+        for ( j = 0; j < voterQualities; j++) {
+            for ( i = 0; i < maxVoters; i++) {
+               if(j == 0){
+                   voters[i][j] = ++EGN;}
+                   else if (j==1){
+                   voters[i][j] = "Pesho";
+               }
+               else if(j==2){
+                   voters[i][j] = String.valueOf((i % 2) == 0 ? Vote.YES : Vote.NO);
+               }
+            }
+        }
     }
 }
 
