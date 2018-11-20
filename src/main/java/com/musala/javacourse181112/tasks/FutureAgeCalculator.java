8622f689497f4ba132ed8a91fe99b99a2a93e1da
Antonio <antoniobuyukliev@yahoo.com>
2018-11-20 18:48:36 +0200

    Vote system 2.2 Upgrade using classes fixed futureAgeCalc

--- src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
@@ -1,9 +1,21 @@
 package com.musala.javacourse181112.tasks;
 
+import java.util.Scanner;
+
 public class FutureAgeCalculator {
     public static void main(String[] args) {
-        System.out.println("Input age: " + args[0]);
-        System.out.println("Input period: " + args[1]);
-        System.out.println("Age in future: " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter current age:");
+        int age = scan.nextInt();
+        System.out.println("Enter period if you want to stop enter '0'");
+        int period = scan.nextInt();
+        int futureAge = age;
+        while (period != 0){
+            futureAge+=period;
+            period = scan.nextInt();
+        }
+        System.out.println("Input age: " + age);
+        System.out.println("Input period: " + (futureAge - age));
+        System.out.println("Age in future: " + futureAge);
     }
 }
