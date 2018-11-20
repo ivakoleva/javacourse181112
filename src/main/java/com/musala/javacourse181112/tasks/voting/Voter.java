d5582fb5e2c9484ba60664f0246f76328d10c1df
IcouldEsmileof <aykut@mail.bg>
2018-11-20 17:43:42 +0200

    updated voting

--- src/main/java/com/musala/javacourse181112/tasks/voting/Voter.java
+++ src/main/java/com/musala/javacourse181112/tasks/voting/Voter.java
@@ -5,18 +5,19 @@ public class Voter {
     public String EGN;
     public Votes[] votes;
     public Voter(){
-        name=null;
-        EGN=null;
-        votes=null;
+
     }
     public Voter(String name,String EGN,Votes[] votes)
     {
-        this.name=name;
-        this.EGN=EGN;
-        this.votes=new Votes[votes.length];
-        for(int i=0;i<votes.length;i++)
+        if(EGN.length()==10)
         {
-            this.votes[i]=votes[i];
+            this.name=name;
+            this.EGN=EGN;
+            this.votes=new Votes[votes.length];
+            for(int i=0;i<votes.length;i++)
+            {
+                this.votes[i]=votes[i];
+            }
         }
     }
 }
