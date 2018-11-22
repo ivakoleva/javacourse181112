fb7a462ae8d79c05510f6e68eae45d697b115279
anton kolev <antongtkolev@gmail.com>
2018-11-22 16:05:28 +0200

    Voting App v0.1

--- src/main/java/com/musala/javacourse181112/VotingApp.java
+++ src/main/java/com/musala/javacourse181112/VotingApp.java
@@ -6,30 +6,27 @@ public class VotingApp {
         // welcome message
         System.out.println("Please vote now ,answer the following questions ");
 
-        // 2 questions,30 voters
-        final String[][] votersArr = new String[2][30];
+        // 2 questions,3 voters
+        final String[][] votersArr = new String[2][3];
 
-        // hardcode 3 person votes (voted yes/no) for the 2 questions
-        String person1 = "Yes";
-        String person2 = "No";
-        String person3 = "No";
-        String person4 = "No";
-        String person5 = "Yes";
-        String person6 = "Yes";
+        // hardcode  person votes (voted yes/no) for the 2 questions
+        String votersAnswer1 = "Yes";
+        String votersAnswer2 = "No";
+
 
         //assign votes to array
-        votersArr[0][0] = person1;
-        votersArr[0][1] = person2;
-        votersArr[0][2] = person3;
-        votersArr[1][0] = person4;
-        votersArr[1][1] = person5;
-        votersArr[1][2] = person6;
+        votersArr[0][0] = votersAnswer1;
+        votersArr[0][1] = votersAnswer2;
+        votersArr[0][2] = votersAnswer1;
+        votersArr[1][0] = votersAnswer2;
+        votersArr[1][1] = votersAnswer2;
+        votersArr[1][2] = votersAnswer1;
 
         //question 1
         System.out.println("Did you like football ?  ");
 
         for (int i = 0; i <= votersArr.length ; i++) {
-            System.out.println(" " + votersArr[0][i]);
+            System.out.println(i+" " + votersArr[0][i]);
         }
         //question 2
         System.out.println("Did you play football ? ");
