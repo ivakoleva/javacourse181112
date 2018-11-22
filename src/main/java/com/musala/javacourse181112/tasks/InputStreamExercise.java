1e7b80c83997b8b3b49718e403d0c3d95aa7ac36
IcouldEsmileof <aykut@mail.bg>
2018-11-22 20:20:39 +0200

    IO update

--- src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
@@ -21,15 +21,20 @@ public class InputStreamExercise {
          * Important note: first 3 bytes are system-reserved, please do not overwrite
          */
         final byte[] buffer = new byte[1027];
-
+        buffer[0]=(byte)'a';
+        buffer[1]=(byte)'b';
+        buffer[2]=(byte)'c';
         /*while ((i = inputStream.read(buffer)) != -1) {
             System.out.print(new String(buffer, 0, i)); // , Charset.defaultCharset()
         }*/
 
         // TODO: implement inputStream.read(buffer, offset, length)
         // TODO: have in mind the Important note above
+        while ((i = inputStream.read(buffer,3,buffer.length-3)) != -1) {
+            System.out.print(new String(buffer, 3, i-3)); // , Charset.defaultCharset()
+        }
 
-
+        System.out.println("\n"+(char)buffer[0]+" "+(char)buffer[1]+" "+(char)buffer[2]);
         // we are required to close, always do remember pls
         inputStream.close();
     }
