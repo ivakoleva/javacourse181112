19fa41b8d8b172afd45910af9f9b55ae4dc3ff67
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-22 16:44:25 +0200

    code review

--- src/main/java/com/musala/javacourse181112/Loops.java
+++ src/main/java/com/musala/javacourse181112/Loops.java
@@ -1,7 +1,7 @@
 package com.musala.javacourse181112;
 
 public class Loops {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         loopForEach();
         loopDoWhile();
         loopWhile();
@@ -20,45 +20,44 @@ public class Loops {
     public static void loopDoWhile() {
         int i = 0;
         int n = 5;
-        int[] array = new int[n];
+        final int[] array = new int[n];
 
         do {
-            array[i] = i;
-            i++;
-        }while (i < n);
+            array[i] = i++;
+            //i++;
+        } while (i < n);
         i = 0;
 
         do {
-            System.out.println(array[i]);
-            i++;
-        }while (i < n);
+            System.out.println(array[i++]);
+            //i++;
+        } while (i < n);
 
         System.out.println();
-
     }
 
     public static void loopWhile() {
         int i = 0;
         int n = 5;
-        int[] array = new int[n];
+        final int[] array = new int[n];
 
         while (i < n) {
-            array[i] = i;
-            i++;
+            array[i] = i++;
+            //i++;
         }
+
         i = 0;
         while (i < n) {
-            System.out.println(array[i]);
-            i++;
+            System.out.println(array[i++]);
+            //i++;
         }
 
         System.out.println();
-
     }
 
     public static void loopFor() {
         int n = 5;
-        int[] array = new int[n];
+        final int[] array = new int[n];
 
         for (int i = 0; i < n; i++) {
             array[i] = i;
