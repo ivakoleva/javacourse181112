09203e1028a5c0a8cc9fb10739e01c2483c04a2b
IcouldEsmileof <aykut@mail.bg>
2018-11-22 19:18:49 +0200

    Merge remote-tracking branch 'origin/master' into Aykut.Ismailov
    # Conflicts:
    #	src/main/java/com/musala/javacourse181112/basics/Gender.java
    #	src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalculator.java
    #	src/main/java/com/musala/javacourse181112/tasks/TernaryRunner.java

--- src/main/java/com/musala/javacourse181112/tasks/LoopExersise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LoopExersise.java
@@ -9,66 +9,69 @@ import java.util.List;
 public class LoopExersise {
     public static void main(String[] args) {
         //usingWhile();
-       // usingDoWhile();
-       // usingFor();
-       // usingForEach();
+        //usingDoWhile();
+        //usingFor();
+        //usingForEach();
         usingLambdaAndIterator();
     }
 
     public static void usingWhile() {
-        int []ar = new int[4];
-        int i=0;
-        while(i< ar.length){
-            ar[i]=i;
+        int[] ar = new int[4];
+        int i = 0;
+        while (i < ar.length) {
+            ar[i] = i;
             i++;
         }
-        i=0;
-        while(i< ar.length){
+        i = 0;
+        while (i < ar.length) {
             System.out.println(ar[i]);
             i++;
         }
 
     }
-    
+
     public static void usingDoWhile() {
-        int []ar = new int[4];
-        int i=0;
-        do{
-            ar[i]=i;
+        int[] ar = new int[4];
+        int i = 0;
+        do {
+            ar[i] = i;
             i++;
-        }while(i< ar.length);
-        i=0;
-        do{
+        } while (i < ar.length);
+        i = 0;
+        do {
             System.out.println(ar[i]);
             i++;
-        }while(i< ar.length);
+        } while (i < ar.length);
     }
+
     public static void usingFor() {
-        int []ar = new int[4];
-        for(int i=0;i < ar.length;i++){
-            ar[i]=i;
+        int[] ar = new int[4];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = i;
         }
-        for(int i=0;i< ar.length;i++){
+        for (int i = 0; i < ar.length; i++) {
             System.out.println(ar[i]);
         }
     }
+
     public static void usingForEach() {
-        Integer []ar = new Integer[4];
-        for(int i=0;i<ar.length;i++){
-            ar[i]=i;
+        Integer[] ar = new Integer[4];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = i;
         }
-        for(int i1:ar){
+        for (int i1 : ar) {
             System.out.println(i1);
         }
     }
-    public static void usingLambdaAndIterator(){
-        List<Integer> ar=new ArrayList<>();
-        for(int i=0;i<4;i++){
+
+    public static void usingLambdaAndIterator() {
+        List<Integer> ar = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
             ar.add(i);
         }
-        for(Iterator<Integer> i=ar.iterator();i.hasNext();){
-            int e=i.next();
-            if(e%2==0) {
+        for (Iterator<Integer> i = ar.iterator(); i.hasNext(); ) {
+            int e = i.next();
+            if (e % 2 == 0) {
                 i.remove();
             }
         }
