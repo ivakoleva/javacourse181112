af1a6e0c6f1eef40b806d3f8bb90d8ca6ad7b961
Galq <galina.sansarowa@gmail.com>
2018-11-22 16:38:15 +0200

    Loops

--- src/main/java/com/musala/javacourse181112/loops.java
+++ src/main/java/com/musala/javacourse181112/loops.java
@@ -3,30 +3,30 @@ package com.musala.javacourse181112;
 
 public class loops {
     public static void main(String[] args) {
-        int value[] = {1,2,3,};
+        int value[] = {1,2,3};
         usingWhile(value);
-        usingDoWhile(value);
         usingFor(value);
         usingForeach(value);
+        usingDoWhile(value);
 
     }
 
 
     public static void usingFor(int[] valueFor) {
         for(int limiterFor=0;limiterFor<valueFor.length;limiterFor++){
-            System.out.println("Printing in For: " + valueFor);
+            System.out.println("Printing in For: " + valueFor[limiterFor]);
         }
     }
 
-    private static void usingDoWhile(int[] valueDoWhile) {
+    public static void usingDoWhile(int[] valueDoWhile) {
         int limiterDoWhile=0;
         do {
-           ++limiterDoWhile;
+            System.out.println("Printing in Do-while: " + valueDoWhile[limiterDoWhile]);
+            limiterDoWhile++;
 
 
         }while(limiterDoWhile<valueDoWhile.length);{
-            System.out.println("Printing in Do-while: " + valueDoWhile);
-            limiterDoWhile++;
+
         }
 
 
@@ -35,15 +35,13 @@ public class loops {
     public static void usingWhile(int[] valueWhile) {
         int limiter = 0;
         while (limiter < valueWhile.length) {
-            System.out.println("Printing in While: " + valueWhile);
+            System.out.println("Printing in While: " + valueWhile[limiter]);
             limiter++;
 
         }
     }
     public static void usingForeach(int [] valueForeach){
-        for(int limiterForeach:valueForeach){
-            System.out.println("Printing in Foreach: " + valueForeach);
-        }
+        for(int limiterForeach:valueForeach) System.out.println("Printing in Foreach: " + limiterForeach);
 
     }
 }
