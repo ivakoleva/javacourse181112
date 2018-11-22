b3ab15d819954fa174db778c5aadf922e78537a3
Vessy <veselabozheva@gmail.com>
2018-11-22 19:30:06 +0200

    attempt of using collections (has issues)

--- src/main/java/com/musala/javacourse181112/tasks/Looper.java
+++ src/main/java/com/musala/javacourse181112/tasks/Looper.java
@@ -1,5 +1,8 @@
 package com.musala.javacourse181112.tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Looper {
@@ -13,6 +16,8 @@ print the array to stdout
     forLoop();
     doWhileLoop();
     whileLoop();
+    useStream();
+    useCollection();
     }
     public static int[] forEachLoop(){
         for ( int num : numbers ){
@@ -50,8 +55,46 @@ print the array to stdout
         }
         return numbers;
     }
+    //todo: elaborate
+    /* add lambda stream.foreach implementation
+    add looping and modifying a collection of strings (using iterator)
+    remve if: element length % 2 == 0
+
+     */
+    // "numbers"array is empty, that's why it doesn't print anything, wanted to just apply the logic if it's correct
+    public static void useStream(){
+        Arrays.stream(numbers).forEach(number -> System.out.println(number));
+
+
+    }
+    //has some issue, don't know what, doesn't get to executing this method when run
+    public static void useCollection(){
+        ArrayList<String> stringCollection = new ArrayList<>();
+        stringCollection.add("1");
+        stringCollection.add("2");
+        stringCollection.add("3");
+        if(stringCollection.isEmpty()){
+            System.out.println(" Your collection is empty");
+        }else{
+            Iterator<String> iterator = stringCollection.iterator();
+
+            while (iterator.hasNext()) {
+                int itr = Integer.parseInt(iterator.next());
+                if(itr % 2 == 0) {
+                     iterator.remove();
+                }
+  }
+                /*for (String s : stringCollection){
+                    if(s.length() % 2 == 0 ){
+                        stringCollection.remove(s);
+                    }
+                }*/
+            }
+        }
+
+    }
 
 
 
-}
+
 
