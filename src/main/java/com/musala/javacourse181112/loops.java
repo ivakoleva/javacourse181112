fd976e238ed19023b9f56dcbc86328d7a90a3685
Galq <galina.sansarowa@gmail.com>
2018-11-25 00:05:11 +0200

    InputStream Exercise

--- src/main/java/com/musala/javacourse181112/loops.java
+++ src/main/java/com/musala/javacourse181112/loops.java
@@ -1,78 +1,68 @@
 package com.musala.javacourse181112;
 
-import java.util.Iterator;
-import java.util.ListIterator;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class loops {
-    public static void main(String[] args) {
-        int value[] = {1,2,3};
+public class Loops {
+    public static void main(final String[] args) {
+        final int value[] = {1, 2, 3};
         usingWhile(value);
         usingFor(value);
         usingForeach(value);
         usingDoWhile(value);
-        usingLandaForeach();
+        usingLamdaForeach();
         usingIterator();
-
     }
 
-
-    public static void usingFor(int[] valueFor) {
-        for(int limiterFor=0;limiterFor<valueFor.length;limiterFor++){
-            System.out.println("Printing in For: " + valueFor[limiterFor]);
+    public static void usingFor(final int[] valueArray) {
+        for (int i = 0; i < valueArray.length; i++) {
+            System.out.println("Printing in For: " + valueArray[i]);
         }
     }
 
-    public static void usingDoWhile(int[] valueDoWhile) {
-        int limiterDoWhile=0;
+    public static void usingDoWhile(final int[] valueArray) {
+        int i = 0;
         do {
-            System.out.println("Printing in Do-while: " + valueDoWhile[limiterDoWhile]);
-            limiterDoWhile++;
-
-
-        }while(limiterDoWhile<valueDoWhile.length);{
-
-        }
-
-
+            System.out.println("Printing in Do-while: " + valueArray[i++]);
+            //i++;
+        } while (i < valueArray.length);
     }
 
-    public static void usingWhile(int[] valueWhile) {
-        int limiter = 0;
-        while (limiter < valueWhile.length) {
-            System.out.println("Printing in While: " + valueWhile[limiter]);
-            limiter++;
-
+    public static void usingWhile(final int[] valueArray) {
+        int i = 0;
+        while (i < valueArray.length) {
+            System.out.println("Printing in While: " + valueArray[i++]);
+            //i++;
         }
     }
-    public static void usingForeach(int [] valueForeach){
-        for(int limiterForeach:valueForeach) System.out.println("Printing in Foreach: " + limiterForeach);
 
+
+    public static void usingForeach(final int[] valueArray) {
+        for (int limiterForeach : valueArray) {
+            System.out.println("Printing in Foreach: " + limiterForeach);
+        }
     }
-    public static void usingLandaForeach(){
-        List<Integer>item=new ArrayList<>();
+
+    public static void usingLamdaForeach() {
+        final List<Integer> item = new ArrayList<>();
         item.add(5);
         item.add(4);
         item.add(3);
-        item.forEach( System.out::println);
+        item.forEach(System.out::println);
     }
-    public static void usingIterator(){
-        List<String>item=new ArrayList<>();
-        item.add("aj");
-        item.add("b");
-        item.add("c");
-        Iterator<String> iterator=item.iterator();
-        while( iterator.hasNext()){
 
-
-
-            if((iterator.next().length()%2==0)){
+    public static void usingIterator() {
+        final List<String> list = new ArrayList<>();
+        list.add("aj");
+        list.add("b");
+        list.add("c");
+        final Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if ((iterator.next().length() % 2 == 0)) {
                 iterator.remove();
             }
-
         }
-        System.out.println(item);
-     }
+        System.out.println(list);
+    }
 }
-
--- src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
@@ -8,6 +8,11 @@ import java.io.InputStream;
  * Created by Iva Koleva on 22.11.2018
  */
 public class InputStreamExercise {
+
+    private static final int OFFSET = 3;
+    private static final int BUFFER_SIZE = 1027;
+    private static final int END_OF_FILE = -1;
+
     public static void main(final String[] args) throws IOException {
         // TODO: try-catch later. ! It is really important, to do try-catch.
 
@@ -20,11 +25,21 @@ public class InputStreamExercise {
         /**
          * Important note: first 3 bytes are system-reserved, please do not overwrite
          */
-        final byte[] buffer = new byte[1027];
+        final byte[] buffer = new byte[BUFFER_SIZE];
+        buffer[0] = (byte) 'a';
+        buffer[1] = (byte) 'b';
+        buffer[2] = (byte) 'c';
 
-        /*while ((i = inputStream.read(buffer)) != -1) {
+        // buffer length should be lower or equal to the offset
+        while ((i = inputStream.read(buffer, OFFSET, buffer.length-OFFSET)) != END_OF_FILE) {
             System.out.print(new String(buffer, 0, i)); // , Charset.defaultCharset()
-        }*/
+
+        }
+        // proof
+        System.out.println("\nPROOF: ");
+        System.out.println((char)buffer[0]);
+        System.out.println((char)buffer[1]);
+        System.out.println((char)buffer[2]);
 
         // TODO: implement inputStream.read(buffer, offset, length)
         // TODO: have in mind the Important note above
