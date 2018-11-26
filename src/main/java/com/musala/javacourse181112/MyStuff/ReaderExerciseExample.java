6cfb01e082c5c7d2465b9e74e88f8db9bffd9f41
Kristina <miss.krisi@abv.bg>
2018-11-26 18:26:02 +0200

    Reader Exercise

---
+++ src/main/java/com/musala/javacourse181112/MyStuff/ReaderExerciseExample.java
@@ -0,0 +1,26 @@
+package com.musala.javacourse181112.MyStuff;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+/*
+*  Reader exercise
+*  read characters from task md  using file reader
+*  write to stdout, optimise(take a look ay parameters available for
+*  systems.out.println(), placing a pointer at () then ctrl+p)
+*
+* */
+
+public class ReaderExerciseExample {
+    public static void main(String[] args) throws IOException {
+        int i;
+        final char buffer[]= new char[1024];
+        final Reader reader= new FileReader("tasks.md");
+        while((i = reader.read(buffer))!= -1){
+            System.out.println(new String(buffer));
+        }
+        reader.close();
+    }
+}
--- src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
@@ -1,8 +1,6 @@
 package com.musala.javacourse181112.tasks;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Created by Iva Koleva on 22.11.2018
@@ -12,24 +10,28 @@ public class InputStreamExercise {
         // TODO: try-catch later. ! It is really important, to do try-catch.
 
         final InputStream inputStream = new FileInputStream("sketch.txt");
+        final OutputStream outputStream = new FileOutputStream("tasks_copy.md");
 
         /**
          * Important note: first 3 bytes are system-reserved, please do not overwrite
+         *
+         *  InputStream exercise v0.2
+         *     ** after reading the file, do store file content to tasks_copy.md
+         *     (to a new file, using FileOutputStream)
          */
         final byte[] buffer = new byte[1027];
         buffer[0] = (byte) 'a';
         buffer[1] = (byte) 'b';
         buffer[2] = (byte) 'c';
 
-        int i;
+        int i, o;
         while ((i = inputStream.read(buffer, 3, buffer.length - 3)) != -1) {
             // print to stdout
             System.out.print(new String(buffer, 3, i)); // , Charset.defaultCharset()
-
+            outputStream.write(buffer,3,buffer.length -3);
             // write to file
             // TODO: do write to a previously instantiated fileOutputStream as well
         }
-
         // proof
         System.out.println((char) buffer[0]);
         System.out.println((char) buffer[1]);
