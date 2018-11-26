fea3c10898cb1031c1026f88b4cd1dc136d89bb7
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-26 16:29:54 +0200

    homework review

--- src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
  * Created by Iva Koleva on 22.11.2018
  */
 public class InputStreamExercise {
-
     private static final int OFFSET = 3;
     private static final int BUFFER_SIZE = 1027;
     private static final int END_OF_FILE = -1;
@@ -32,18 +31,14 @@ public class InputStreamExercise {
 
         // buffer length should be lower or equal to the offset
         while ((i = inputStream.read(buffer, OFFSET, buffer.length-OFFSET)) != END_OF_FILE) {
-            System.out.print(new String(buffer, 0, i)); // , Charset.defaultCharset()
-
+            System.out.print(new String(buffer, OFFSET, i)); // , Charset.defaultCharset()
         }
+
         // proof
         System.out.println("\nPROOF: ");
         System.out.println((char)buffer[0]);
         System.out.println((char)buffer[1]);
         System.out.println((char)buffer[2]);
-
-        // TODO: implement inputStream.read(buffer, offset, length)
-        // TODO: have in mind the Important note above
-
 
         // we are required to close, always do remember pls
         inputStream.close();
