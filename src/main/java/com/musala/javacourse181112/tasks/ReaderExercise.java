39d0577a4bb66882bf8dfa8352da9035dca63cde
Mirella Krysteva <mirella.krysteva@gmail.com>
2018-11-26 17:56:35 +0200

    ReaderExercise

---
+++ src/main/java/com/musala/javacourse181112/tasks/ReaderExercise.java
@@ -0,0 +1,18 @@
+package com.musala.javacourse181112.tasks;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReaderExercise {
+    public static void main(final String[] args) throws IOException {
+        final FileReader fileReader = new FileReader("tasks.md");
+        final char[] buffer = new char[1024];
+
+        int i;
+        while ((i = fileReader.read(buffer)) != -1) {
+            System.out.print(new String(buffer));
+        }
+
+        fileReader.close();
+    }
+}
