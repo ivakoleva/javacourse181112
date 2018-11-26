37b0bef685bf1be13d8ee1a7001eb67b0225e63f
IcouldEsmileof <aykut@mail.bg>
2018-11-26 18:58:58 +0200

    new IO update

--- src/main/java/com/musala/javacourse181112/tasks/NewInputOutputExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/NewInputOutputExercise.java
@@ -1,13 +1,14 @@
 package com.musala.javacourse181112.tasks;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
-public class NewInputExercise {
+public class NewInputOutputExercise {
     public static void main(final String[] args) throws IOException {
         final FileReader fileReader = new FileReader("tasks.md");
         final char[] buffer = new char[1024];
-
+        //final FileWriter fileWriter = new FileWriter("new_tasks_copy.md");
         while (fileReader.read(buffer) != -1) {
 
             System.out.print(buffer);
