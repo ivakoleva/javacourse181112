ad869df372d5f61853066d6bf51866e24da9318a
anton kolev <antongtkolev@gmail.com>
2018-11-26 17:15:16 +0200

    Input exercise update

--- src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
@@ -12,14 +12,16 @@ public class InputStreamExercise {
         final InputStream inputStream = new FileInputStream("sketch.txt");
         final OutputStream outputStream = new FileOutputStream("task_copy.md");
         int i;
-        /*while ((i = inputStream.read()) != -1) {
-            System.out.print((char) i);
-        }*/
+
+        while ((i = inputStream.read()) != -1) {
+           outputStream.write(i);
+        }
+
 
         /**
          * Important note: first 3 bytes are system-reserved, please do not overwrite
          */
-        final byte[] buffer = new byte[1027];
+        /*final byte[] buffer = new byte[1027];
         buffer[0] = (byte) 'a';
         buffer[1] = (byte) 'b';
         buffer[2] = (byte) 'c';
@@ -33,9 +35,9 @@ public class InputStreamExercise {
 
 
         // proof
-        System.out.println((char) buffer[0]);
+        /*System.out.println((char) buffer[0]);
         System.out.println((char) buffer[1]);
-        System.out.println((char) buffer[2]);
+        System.out.println((char) buffer[2]);*/
 
         // we are required to close, always do remember pls
         inputStream.close();
