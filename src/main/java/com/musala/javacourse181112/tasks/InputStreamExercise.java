680ea75e1f9a318172cc2fcf126b9cb5f15f8d0f
dgluharov <dgluharov@gmail.com>
2018-11-26 17:16:45 +0200

    InputStream exercise v0.2

--- src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/InputStreamExercise.java
@@ -1,8 +1,6 @@
 package com.musala.javacourse181112.tasks;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Created by Iva Koleva on 22.11.2018
@@ -12,22 +10,17 @@ public class InputStreamExercise {
         // TODO: try-catch later. ! It is really important, to do try-catch.
 
         final InputStream inputStream = new FileInputStream("sketch.txt");
+        final OutputStream outputStream = new FileOutputStream("tasks_copy.md");
 
         /**
          * Important note: first 3 bytes are system-reserved, please do not overwrite
          */
-        final byte[] buffer = new byte[1027];
+        //final byte[] buffer = new byte[1027];
 
         int i;
-        while ((i = inputStream.read(buffer, 3, buffer.length - 3)) != -1) {
-            // print to stdout
-            System.out.print(new String(buffer, 3, i)); // , Charset.defaultCharset()
-
-            // write to file
-            // TODO: do write to a previously instantiated fileOutputStream as well
+        while ((i = inputStream.read()) != -1) {
+            outputStream.write(i);
         }
-
-        // we are required to close, always do remember pls
         inputStream.close();
     }
 }
