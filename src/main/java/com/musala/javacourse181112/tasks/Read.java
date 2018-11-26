31f3ffdc5665373405332dbe9997ee01b483485a
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-26 18:24:31 +0200

    code review

--- src/main/java/com/musala/javacourse181112/tasks/Read.java
+++ src/main/java/com/musala/javacourse181112/tasks/Read.java
@@ -1,17 +1,16 @@
 package com.musala.javacourse181112.tasks;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Read {
-    public static void main(String[] args) throws IOException {
-        final FileReader fileReader=new FileReader("tasks.md");
+    public static void main(final String[] args) throws IOException {
+        final FileReader fileReader = new FileReader("tasks.md");
         int i;
-        final char buffer []= new char[1024];
+        final char buffer[] = new char[1024];
 
-        while ((i = fileReader.read(buffer,0,buffer.length)) != -1){
-            System.out.println(new String(buffer,0,i));
+        while ((i = fileReader.read(buffer)) != -1) {
+            System.out.println(new String(buffer, 0, i));
 
         }
         fileReader.close();
