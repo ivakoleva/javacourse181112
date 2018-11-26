51f3441a8a882940de920c86e555083c78b2caa2
YordanRadkov <iordanradkov@gmail.com>
2018-11-26 18:08:59 +0200

    Reader exercise

--- src/main/java/com/musala/javacourse181112/tasks/ReaderExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/ReaderExercise.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 public class ReaderExercise {
     public static void main(final String[] args) throws IOException {
         final FileReader fileReader = new FileReader("tasks.md");
-        final char[] buffer = new char[1024];
+        //final char[] buffer = new char[1024];
         int i;
-        while ((i = fileReader.read(buffer)) != -1) {
+        while ((i = fileReader.read()) != -1) {
             // test & fix
-            System.out.print(new String(buffer));
+            System.out.print((char)i);
         }
         fileReader.close();
     }
