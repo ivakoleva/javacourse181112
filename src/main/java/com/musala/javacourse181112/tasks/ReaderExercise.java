cd570cc8e7cf6d2b2b606f291f4be6496b159231
Vessy <veselabozheva@gmail.com>
2018-11-26 17:55:14 +0200

    Reader Exercise

--- src/main/java/com/musala/javacourse181112/tasks/ReaderExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/ReaderExercise.java
@@ -1,4 +1,21 @@
 package com.musala.javacourse181112.tasks;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class ReaderExercise {
+    // read characters from tasks.md, using FileReader
+    //write to stdout, optimize
+
+    public static void main(String[] args) throws IOException {
+        final Reader reader = new FileReader("tasks.md");
+        int data = reader.read();
+        while (data != -1){
+            char dataChar = (char) data;
+            data = reader.read();
+            System.out.print(dataChar);
+        }
+        reader.close();
+    }
 }
