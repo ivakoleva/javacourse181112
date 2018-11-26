e4425edc1102835bd6da6abde40c10c8dc29c7bf
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-26 17:57:03 +0200

    code review

--- src/main/java/com/musala/javacourse181112/tasks/ReaderExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/ReaderExercise.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class ReaderExercise {
     public static void main(final String[] args) throws IOException {
         final FileReader fileReader = new FileReader("tasks.md");
-        final char[] buffer = new char[1027];
+        final char[] buffer = new char[1024];
         int i;
         while ((i = fileReader.read(buffer)) != -1) {
             // test & fix
