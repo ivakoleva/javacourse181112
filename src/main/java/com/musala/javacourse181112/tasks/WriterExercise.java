20bd5a52c9ececa66a5ac27f527645367ba0a105
Blueshadows <blueshadows98@gmail.com>
2018-11-27 21:26:50 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/WriterExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/WriterExercise.java
@@ -2,15 +2,16 @@ package com.musala.javacourse181112.tasks;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Writer;
 
 public class WriterExercise {
 
     public static void main(final String[] args) {
 
-        try( final Writer writer=new FileWriter("test.txt")) {
+        try(final PrintWriter writer=new PrintWriter(new FileWriter("test.txt"))) {
 for(int i=0;i<args.length;i++){
-    writer.write(args[i]);
+    writer.println(args[i]);
 
 }
 
