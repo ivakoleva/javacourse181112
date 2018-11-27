0fa98f57a93b79118df8770932aad7073d840d5f
IcouldEsmileof <aykut@mail.bg>
2018-11-27 20:18:38 +0200

    new IO update

--- src/main/java/com/musala/javacourse181112/tasks/NewInputOutputExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/NewInputOutputExercise.java
@@ -1,5 +1,8 @@
 package com.musala.javacourse181112.tasks;
 
+import com.sun.org.apache.xml.internal.serialize.LineSeparator;
+
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,15 +10,31 @@ import java.io.IOException;
 public class NewInputOutputExercise {
 
     public static void main(final String[] args) throws IOException {
-        final FileReader fileReader = new FileReader("tasks.md");
-        final char[] buffer = new char[1024];
-        //final FileWriter fileWriter = new FileWriter("new_tasks_copy.md");
-        while (fileReader.read(buffer) != -1) {
+        usingReaderClass();
+        usingWriterClass(args);
+    }
 
+    private static void usingReaderClass() {
 
-            System.out.print(buffer);
+        try (final FileReader fileReader = new FileReader("tasks.md")) {
+            final char[] buffer = new char[1024];
+            while (fileReader.read(buffer) != -1) {
+                System.out.print(buffer);
             }
-        System.out.println();
-        fileReader.close();
+            System.out.println();
+        } catch (IOException ignored) {
+        }
+    }
+
+    private static void usingWriterClass(final String[] args) {
+
+        try (final FileWriter fileWriter = new FileWriter("WritingParameters.txt");) {
+            for (String s : args) {
+                fileWriter.write(s + "\n");   //version 1: 1 lined
+                //fileWriter.write(s);           //version 2: 2 lined
+                //fileWriter.write("\n");
+            }
+        } catch (IOException ignored) {
+        }
     }
 }
