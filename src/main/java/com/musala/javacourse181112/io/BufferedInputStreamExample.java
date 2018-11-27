e81237b5c01918f7295469366a4d6fda08e02fe6
YordanRadkov <iordanradkov@gmail.com>
2018-11-27 19:51:54 +0200

    BufferedOutputStream PushbackReader Exercise

--- src/main/java/com/musala/javacourse181112/io/BufferedInputStreamExample.java
+++ src/main/java/com/musala/javacourse181112/io/BufferedInputStreamExample.java
@@ -1,17 +1,21 @@
 package com.musala.javacourse181112.io;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Created by Iva Koleva on 26.11.2018
  */
 public class BufferedInputStreamExample {
     public static void main(String[] args) {
-        try (final InputStream inputStream = new BufferedInputStream(new FileInputStream("tasks.md"), 128)) {
-            printInputStreamToStdout(inputStream);
+        try (final InputStream inputStream = new BufferedInputStream(new FileInputStream("tasks.md"), 128);
+             final OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("bufferOutputFile.md"),128)
+        ) {
+            //printInputStreamToStdout(inputStream);
+            int i;
+            while ((i = inputStream.read()) != -1) {
+                System.out.print((char) i);
+                outputStream.write((char) i);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
