cb2b8813b26e99a96ae593c8763dccfc5beb4220
IcouldEsmileof <aykut@mail.bg>
2018-11-27 22:22:38 +0200

    FilterInputStream update

--- src/main/java/com/musala/javacourse181112/tasks/MyFilterInputStream.java
+++ src/main/java/com/musala/javacourse181112/tasks/MyFilterInputStream.java
@@ -8,15 +8,7 @@ public class MyFilterInputStream extends FilterInputStream {
 
     private int count;
 
-    /**
-     * Creates a <code>FilterInputStream</code>
-     * by assigning the  argument <code>in</code>
-     * to the field <code>this.in</code> so as
-     * to remember it for later use.
-     *
-     * @param in the underlying input stream, or <code>null</code> if
-     *           this instance is to be created without an underlying stream.
-     */
+
     protected MyFilterInputStream(InputStream in) {
         super(in);
     }
