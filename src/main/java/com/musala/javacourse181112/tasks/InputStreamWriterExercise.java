44f683ff8221989f094dad50729920bc04d67613
Blueshadows <blueshadows98@gmail.com>
2018-11-27 23:09:07 +0200

    homework

--- src/main/java/com/musala/javacourse181112/tasks/InputStreamWriterExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/InputStreamWriterExercise.java
@@ -1,4 +1,22 @@
 package com.musala.javacourse181112.tasks;
 
+import java.io.*;
+import java.nio.charset.Charset;
+
 public class InputStreamWriterExercise {
+    public static void main(final String[] args) {
+
+        try(final Writer writer=new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream("test1.txt"),Charset.forName("Windows-1251")),128)
+        ) {
+            writer.write("Проба/Грешка");
+
+        } catch (IOException ignored) {
+
+        }
+
+
+    }
+
 }
