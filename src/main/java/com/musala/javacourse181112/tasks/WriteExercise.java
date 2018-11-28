81b3281bad34cf18482d0d3ee865ff670cabaf31
BorisBNikolov <nikolov.b.boirs@gmail.com>
2018-11-28 02:31:38 +0200

    ReaderExercise

---
+++ src/main/java/com/musala/javacourse181112/tasks/WriteExercise.java
@@ -0,0 +1,15 @@
+package com.musala.javacourse181112.tasks;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteExercise {
+    public static void main(final String[] args) {
+        try (final FileWriter fileWriter = new FileWriter("WritingParameters.txt")){
+            for (String str : args){
+                fileWriter.write(str+"\n");
+            }
+        }catch (IOException ignored){
+        }
+    }
+}
