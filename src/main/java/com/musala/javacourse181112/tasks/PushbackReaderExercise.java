e6906e5166ef149d9ea358e2b4424ba41bb18f3b
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-29 16:32:17 +0200

    PushbackReaderExercise review

--- src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 import java.io.PushbackReader;
 
 public class PushbackReaderExercise {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         try (final PushbackReader pushbackReader = new PushbackReader(new FileReader("tasks.md"), 128)) {
             int i;
             while ((i = pushbackReader.read()) != -1) {
+                // TODO
                 System.out.print((char) i);
             }
-        } catch (
-                IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
