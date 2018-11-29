912f959797ae44755ac13d95eaaeed45a6f31501
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-29 17:25:17 +0200

    pushback reader exercise review

--- src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import java.io.PushbackReader;
 
 public class PushbackReaderExercise {
-
-    public static void main(String[] args) {
-        //PushBackReaderExerciseIdea_brokenVersion();
-        PushBackReaderExerciseIdea_unbrokenVersion();
-
+    public static void main(final String[] args) {
+        pushBackReaderExerciseIdea_brokenVersion();
+        //pushBackReaderExerciseIdea_workingVersion();
+        // TODO: implement 3-line requirements; all pieces are here
     }
 
-    private static void PushBackReaderExerciseIdea_brokenVersion() {
+    private static void pushBackReaderExerciseIdea_brokenVersion() {
         int i;
         int toBeUnread = 3;
         char[] buff = new char[128];
@@ -28,36 +27,34 @@ public class PushbackReaderExercise {
                         pushbackReader.unread(buff[i - k - 1]);
                     }
                 }
-
             }
             System.out.println();
         } catch (IOException ignored) {
         }
     }
 
-    private static void PushBackReaderExerciseIdea_unbrokenVersion() {
+    private static void pushBackReaderExerciseIdea_workingVersion() {
         int i;
         int toBeUnread = 3;
-        char[] buff = new char[128];
-        boolean l=true;
-        try (PushbackReader pushbackReader = new PushbackReader(new FileReader("WritingParameters.txt"), toBeUnread + 1)) {
-            while ((i = pushbackReader.read(buff)) != -1) {
-                System.out.print(buff);
+        final char[] buffer = new char[128];
+        boolean l = true;
+        try (final PushbackReader pushbackReader = new PushbackReader(new FileReader("WritingParameters.txt"), toBeUnread + 1)) {
+            while ((i = pushbackReader.read(buffer)) != -1) {
+                System.out.print(buffer);
                 int j = 0;
                 int c;
-                if((c = pushbackReader.read()) == -1) {
-                    l=false;
-                    System.out.println(buff[i-3]+""+buff[i-2]+""+buff[i-1]);
-                }
-                else {
+                if ((c = pushbackReader.read()) == -1) {
+                    l = false;
+                    System.out.println("" + buffer[i - 3] + buffer[i - 2] + buffer[i - 1]);
+                } else {
                     pushbackReader.unread(c);
                 }
-                while (Character.isWhitespace(buff[i - 1 - j])) {
+                while (Character.isWhitespace(buffer[i - 1 - j])) {
                     j++;
                 }
-                if (buff[i - 1 - j] >= 'a' && buff[i - 1 - j] <= ('a' + 'z') / 2 && l ) {
+                if (buffer[i - 1 - j] >= 'a' && buffer[i - 1 - j] <= ('a' + 'z') / 2 && l) {
                     for (int k = 0; k < toBeUnread + j; k++) {
-                        pushbackReader.unread(buff[i - k - 1]);
+                        pushbackReader.unread(buffer[i - k - 1]);
                     }
                 }
             }
