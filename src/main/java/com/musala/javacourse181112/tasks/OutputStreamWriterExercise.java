20159866cb01ff546aad26126c20c44d0cad0a14
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-29 17:30:07 +0200

    code review

--- src/main/java/com/musala/javacourse181112/tasks/OutputStreamWriterExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/OutputStreamWriterExercise.java
@@ -5,18 +5,12 @@ import java.nio.charset.Charset;
 
 public class OutputStreamWriterExercise {
     public static void main(final String[] args) {
-
-        try(final Writer writer=new BufferedWriter(
+        try (final Writer writer = new BufferedWriter(
                 new OutputStreamWriter(
-                        new FileOutputStream("test1.txt"),Charset.forName("Windows-1251")),128)
+                        new FileOutputStream("test1.txt"), Charset.forName("Windows-1251")), 128)
         ) {
             writer.write("Проба/Грешка");
-
         } catch (IOException ignored) {
-
         }
-
-
     }
-
 }
