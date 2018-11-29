82ecd93d09559ca41e020b7b5b9f8778b8e79823
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-29 17:46:40 +0200

    code review [array out of bound exception fix; todo test]

--- src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
@@ -8,40 +8,46 @@ public class PushbackReaderExercise {
     private static final String INPUT_FILE_NAME = "input.txt";
     private static final int LIMIT = 3;
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         getUsePushbackReader();
     }
 
     private static void getUsePushbackReader() {
-        try(PushbackReader pushbackReader = new PushbackReader(new FileReader(INPUT_FILE_NAME), LIMIT+1);
-        ) {
-            int i,j,repeat = 0;
-            char[] buf2 = new char[LIMIT];
-            final char[] buf = new char[138];
-               while ((i = pushbackReader.read(buf))!=-1){
-                for (j = 0;( buf[j]!='\0' || j < i ); j++) {
-                    System.out.print(buf[j]);
-                    char curChar = buf[j];
+        try (final PushbackReader pushbackReader = new PushbackReader(new FileReader(INPUT_FILE_NAME), LIMIT + 1);) {
+            int i, j, repeat = 0;
+            //char[] buf2 = new char[LIMIT];
+            final char[] buffer = new char[138];
+            while ((i = pushbackReader.read(buffer)) != -1) {
+                for (j = 0; (buffer[j] != '\0' || j < i); j++) {
+                    System.out.print(buffer[j]);
+                    final char curChar = buffer[j];
 
                     if (curChar >= 'a' && curChar <= 'p') {
                         //System.out.println("\n>"+curChar);
                         /*for(int k=0; k <LIMIT; k++){
                             pushbackReader.unread(buf2[i-k+1]);
                         }*/
-                        pushbackReader.unread(buf[j]);
-                        pushbackReader.unread(buf[--j]);
-                        pushbackReader.unread(buf[--j]);
+                        //pushbackReader.unread(buffer, j - 3, 3);
+                        unreadBufferMaxPositions(pushbackReader, buffer, j - 3);
+
                         System.out.println();
                         break;
                     }
-
                 }
-                   if (++repeat > 1){
-                       break ;
-                   }
-               }
+                if (++repeat > 1) {
+                    break;
+                }
+            }
         } catch (IOException e) {
-            System.err.println(e.getMessage());;
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void unreadBufferMaxPositions(final PushbackReader pushbackReader, final char[] charsBuffer, final int offset) throws IOException {
+        for (int i = offset; i <= offset + LIMIT; i++) {
+            if (i >= 0 && i < charsBuffer.length) {
+                pushbackReader.unread(charsBuffer[i]);
+            }
         }
     }
 }
