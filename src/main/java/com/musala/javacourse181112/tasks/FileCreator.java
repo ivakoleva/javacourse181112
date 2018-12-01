b1667142e7a9d59fb17c496099e544558ad7333c
VermilionPride <dimitar.ang.mgu@gmail.com>
2018-12-01 10:38:48 +0200

    File creation

--- src/main/java/com/musala/javacourse181112/tasks/FileCreator.java
+++ src/main/java/com/musala/javacourse181112/tasks/FileCreator.java
@@ -1,12 +1,21 @@
 package com.musala.javacourse181112.tasks;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileCreator {
     public static void main(String[] args) throws IOException {
-        final File file = new File("src","voting.txt");
+        final File file = new File("src","file.txt");
         file.createNewFile();
+        final OutputStream outputStream=new FileOutputStream("file.txt");
+        final byte[] buffer = new byte[1027];
+        buffer[0]=(byte)'a';
+        buffer[1]=(byte)'b';
+        buffer[2]=(byte)'c';
+        int i;
+        final FileInputStream inputStream = new FileInputStream("tasks.md");
+        while ((i = inputStream.read(buffer,3,buffer.length-3)) != -1) {
+            outputStream.write(buffer,3,i);
+        }
         System.out.println("file:" + file.exists());
     }
 }
