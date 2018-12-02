da0cd32aff1b95ad2eb052ab49d8f2b490244c34
Blueshadows <blueshadows98@gmail.com>
2018-12-02 12:26:56 +0200

    PushbackReader Working

--- src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/PushbackReaderExercise.java
@@ -1,28 +1,31 @@
 package com.musala.javacourse181112.tasks;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PushbackReader;
+
 ///NOT WORKING
 public class PushbackReaderExercise {
     public static void main(final String[] args) {
         int i;
-
-        try(final PushbackReader pushbackReader=new PushbackReader(new FileReader("test.txt"),128)){
-            while((i=pushbackReader.read())!=-1){
-                System.out.print((char)i);
+        final char[] buffer = new char[128];
+        try (final PushbackReader pushbackReader = new PushbackReader(new FileReader("test.txt"), 3)) {
+            while ((i = pushbackReader.read(buffer)) != -1) {
+                System.out.print(buffer);
+                if(buffer[i-1]>='a'&& buffer[i-1]<='p') {
+                    pushbackReader.unread(buffer, i-3 , 3);
+                    i = pushbackReader.read(buffer);
+                    System.out.println(new String(buffer, 0, i));
             }
 
-                pushbackReader.unread((char)i);
-                i=pushbackReader.read();
-                System.out.println(i+"test");
-            } catch (IOException ignored) {
+            }
+
+        } catch (IOException ignored) {
 
         }
 
     }
 
 
-    }
+}
 
