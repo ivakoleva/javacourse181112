c7bad911e446d110734c5a3d794f87df86732c5c
Blueshadows <blueshadows98@gmail.com>
2018-12-02 10:34:05 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/FileExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/FileExercise.java
@@ -1,7 +1,12 @@
 package com.musala.javacourse181112.tasks;
 
+import java.io.File;
+
 public class FileExercise {
     public static void main(final String[] args) {
+        final File file=new File("testpath");
+System.out.println("File exists:"+file.exists());
+file.mkdir();
 
     }
 
--- src/main/java/com/musala/javacourse181112/tasks/OutputStreamWriterExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/OutputStreamWriterExercise.java
@@ -5,12 +5,18 @@ import java.nio.charset.Charset;
 
 public class OutputStreamWriterExercise {
     public static void main(final String[] args) {
-        try (final Writer writer = new BufferedWriter(
-                new OutputStreamWriter(
-                        new FileOutputStream("test1.txt"), Charset.forName("Windows-1251")), 128)
+        try (final Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("test1.txt"),
+                Charset.forName("Windows-1251")), 128)
         ) {
             writer.write("Проба/Грешка");
         } catch (IOException ignored) {
         }
     }
 }
+
+
+/*
+try(){}
+catch{}
+catch{}
+finally{}*/
