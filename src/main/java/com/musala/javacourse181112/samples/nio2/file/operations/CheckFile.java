d1270b3250c13d25edb874bb106484997bd4b333
Hristo Kostov <hkostov@gmail.com>
2018-12-03 15:44:13 +0200

    Second day samples ;).

---
+++ src/main/java/com/musala/javacourse181112/samples/nio2/file/operations/CheckFile.java
@@ -0,0 +1,17 @@
+package com.musala.javacourse181112.samples.nio2.file.operations;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * TODO: Add docs
+ */
+public class CheckFile {
+	public static void main(String[] args) {
+		Path path = Paths.get("pom.xml");
+		boolean exists = Files.exists(path);
+		System.out.println(exists);
+		//System.out.println(Files.notExists(path));
+	}
+}
