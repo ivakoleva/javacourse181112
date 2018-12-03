f10a7ac6ee11795dda80f2b7a56d21434190a217
YordanRadkov <iordanradkov@gmail.com>
2018-12-03 18:21:52 +0200

    LambdaExercise String to StringBulder

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -8,6 +8,20 @@ import java.util.function.Function;
 public class LambdaExercise {
     public static void main(final String[] args) {
         // TODO: implement
-        final Function<String, StringBuilder> stringToStringBuilder;
+        final Function<String, StringBuilder> stringToStringBuilder = text -> {
+            //StringBuilder stringBulder = new StringBuilder(text);
+           // return stringBulder;
+
+            StringBuilder stringBulder = new StringBuilder();
+            for (int i = 0; i < text.length(); i++){
+                stringBulder.append(text.charAt(i));
+            }
+            return stringBulder;
+        };
+
+        final StringBuilder stringBuilder = stringToStringBuilder.apply("Hello");
+        System.out.println(stringBuilder);
+
+        }
     }
-}
+
