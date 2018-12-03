4e6692d1e46b5fa26fa75a62010152d892aab9fc
IcouldEsmileof <aykut@mail.bg>
2018-12-03 17:13:53 +0200

    Lambda Update

--- src/main/java/com/musala/javacourse181112/basics/LambdaExample.java
+++ src/main/java/com/musala/javacourse181112/basics/LambdaExample.java
@@ -26,6 +26,7 @@ public class LambdaExample {
         Arrays.stream(intArray).forEach(i -> System.out.println(i + 1));
         Arrays.stream(intArray).forEach(System.out::println);
         final Function<String,StringBuilder> castStringToStringBuilder = (s)->new StringBuilder(s);
+
         }
 }
 
