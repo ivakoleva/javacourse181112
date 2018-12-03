1138a9c376285963574bd3f733836ed08ac536a3
YordanRadkov <iordanradkov@gmail.com>
2018-12-03 20:05:05 +0200

    LambdaExercise String to StringBuilder

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -8,6 +8,18 @@ import java.util.function.Function;
 public class LambdaExercise {
     public static void main(final String[] args) {
         // TODO: implement
-        final Function<String, StringBuilder> stringToStringBuilderFunction;
+
+        final Function<String, StringBuilder> stringToStringBuilder = text -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            //StringBuilder stringBuilder1 = new StringBuilder(text);
+            for (int i =0; i < text.length(); i++){
+                stringBuilder.append(text.charAt(i));
+            }
+            return stringBuilder;
+        } ;
+
+        final StringBuilder stringBuilder = stringToStringBuilder.apply("Hello");
+        System.out.println(stringBuilder);
+
     }
 }
