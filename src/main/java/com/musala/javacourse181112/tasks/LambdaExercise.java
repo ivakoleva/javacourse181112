b999554f5000a4c463745c4213b58937a1eef3d0
muzun97 <muzun@abv.bg>
2018-12-04 20:26:34 +0200

    LambdaExercise

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -1,6 +1,7 @@
 package com.musala.javacourse181112.tasks;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Created by Iva Koleva on 03.12.2018
@@ -9,5 +10,8 @@ public class LambdaExercise {
     public static void main(final String[] args) {
 
         final Function<String, StringBuilder> stringToStringBuilder = (i) -> new StringBuilder(i);
+
+        final Predicate<String> lengthString = s -> s.length() == 10;
+
     }
 }
