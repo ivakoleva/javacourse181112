d46895e25c3a97e792abc17090f21a325a4f4c49
YordanRadkov <iordanradkov@gmail.com>
2018-12-04 16:20:37 +0200

    Lambda exercise:    implement String to StringBuilder function    implement String predicate - if string is length of 10

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -1,6 +1,7 @@
 package com.musala.javacourse181112.tasks;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Created by Iva Koleva on 03.12.2018
@@ -21,5 +22,8 @@ public class LambdaExercise {
         final StringBuilder stringBuilder = stringToStringBuilder.apply("Hello");
         System.out.println(stringBuilder);
 
+        final Predicate<String> lengthOfString = s -> s.length() == 10;
+        System.out.println(lengthOfString.test("0123456789"));
+        //System.out.println(lengthOfString.negate().test("0123456789"));// return !(test())
     }
 }
