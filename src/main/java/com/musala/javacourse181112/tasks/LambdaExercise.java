e0709d0915304126c6c9cfe2740bda1fc62f9406
Blueshadows <blueshadows98@gmail.com>
2018-12-04 23:03:46 +0200

    LambdaExercise

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -1,13 +1,17 @@
 package com.musala.javacourse181112.tasks;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Created by Iva Koleva on 03.12.2018
  */
 public class LambdaExercise {
     public static void main(final String[] args) {
-        // TODO: implement
+
         final Function<String, StringBuilder> stringToStringBuilderFunction= StringBuilder::new;
+        final Predicate<String> stringTestLenght10=s-> s!=null && s.length()==10;
+       System.out.println( stringTestLenght10.test("0123456789"));
+
     }
 }
