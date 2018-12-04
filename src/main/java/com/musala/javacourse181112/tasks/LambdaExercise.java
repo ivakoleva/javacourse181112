a77e99468609481ad97591553015499cb7e66887
dgluharov <dgluharov@gmail.com>
2018-12-04 17:06:10 +0200

    Exercises

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -6,8 +6,12 @@ import java.util.function.Function;
  * Created by Iva Koleva on 03.12.2018
  */
 public class LambdaExercise {
+    private static final Object StringBuilder = new StringBuilder();
     public static void main(final String[] args) {
         // TODO: implement
-        final Function<String, StringBuilder> stringToStringBuilderFunction;
+        final Function<String, StringBuilder> stringToStringBuilderFunction = String -> StringBuilder;
+        final String result = stringToStringBuilderFunction.apply(1);
+
+
     }
 }
