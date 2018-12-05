1eaa3bf2d5f3502ab9cab8b9e90050000eeef9fc
anton kolev <antongtkolev@gmail.com>
2018-12-05 13:03:27 +0200

    Primitives and Wrappers exercise (All tasks)

--- src/main/java/com/musala/javacourse181112/io/FileExercise.java
+++ src/main/java/com/musala/javacourse181112/io/FileExercise.java
@@ -1,4 +1,16 @@
 package com.musala.javacourse181112.io;
 
+import java.io.*;
+
 public class FileExercise {
+    public static void main(final String[] args) throws IOException {
+        final File file = new File("src", "test");
+        file.createNewFile();
+        System.out.println("File " + file.exists());
+        if(!file.mkdir()) {
+            final File file1 =new File(file,"file1");
+        }
+    }
+
+
 }
---
+++ src/main/java/com/musala/javacourse181112/tasks/PrimitivesDataExercise.java
@@ -0,0 +1,75 @@
+package com.musala.javacourse181112.tasks;
+
+import java.util.*;
+import java.lang.*;
+import java.util.stream.Stream;
+
+public class PrimitivesDataExercise {
+    public static void main(final String[] args) {
+
+        byte byteNum = 1;
+        short shortNum = 13;
+        int intNum = 120;
+        long longNum = 304L;
+        float floatNum = 2.3f;
+        double doubleNum = 13.788d;
+        char charValue = 'c';
+        boolean booleanVavlue = true;
+
+        Byte byteWrapp = byteNum;
+        Short shortWrapp = shortNum;
+        Integer intWrapp = intNum;
+        Long longWrapp = longNum;
+        Float floatWrapp = floatNum;
+        Double doubleWrapp = doubleNum;
+        Character charWrapp = charValue;
+        Boolean booleanWrapp = true;
+
+
+        Object[] arrayPrimitives = new Object[7];
+        arrayPrimitives[0] = byteWrapp;
+        arrayPrimitives[1] = shortWrapp;
+        arrayPrimitives[2] = intWrapp;
+        arrayPrimitives[3] = floatWrapp;
+        arrayPrimitives[4] = doubleWrapp;
+        arrayPrimitives[5] = charWrapp;
+        arrayPrimitives[6] = true;
+
+        System.out.println("Array of Primitives : " );
+        printArrayPrimitives(arrayPrimitives);
+
+        System.out.println(System.lineSeparator() + "\nStream of the Array : ");
+
+
+
+        try (Stream<Object> streamData = Arrays.stream(arrayPrimitives)) {
+            streamData.forEach(varData -> System.out.print(varData + " "));
+        }
+        System.out.println(System.lineSeparator() + "\nBoolean filtered :");
+        BooleanFilter(arrayPrimitives);
+
+    }
+
+
+    public static void printArrayPrimitives(final Object... arrayPrimitives) {
+
+        for (Object varData : arrayPrimitives) System.out.print(varData + " ");
+
+
+    }
+
+
+    public static void BooleanFilter ( final Object... arrayPrimitives){
+        List<Object> arrayList = new ArrayList<>();
+        for (Object varData : arrayPrimitives) {
+            if ( varData instanceof Boolean){
+                arrayList.add(varData);
+            }
+
+        }
+
+        for (Object varData : arrayList) System.out.println(varData + " ");
+    }
+
+}
+
