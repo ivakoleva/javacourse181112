85bc90b0876b20b835f87c9eb603d74369569c3e
Bzahov98 <bzahov1998@gmail.com>
2018-12-06 16:56:50 +0200

    [ADD] PrimitiveAndWrapper + StringPredicate

---
+++ src/main/java/com/musala/javacourse181112/tasks/StringPredicateExample.java
@@ -0,0 +1,19 @@
+package com.musala.javacourse181112.tasks;
+
+import java.util.function.Predicate;
+
+public class StringPredicateExample {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder();
+        System.out.println(sb.capacity());
+        sb.append("Hello");
+        System.out.println(sb.capacity());
+        sb.append("World");
+        System.out.println(sb.capacity());
+
+        Predicate<String> i  = (s)-> s.length() == 10;
+        System.out.println(i.test(sb.toString()));
+        sb.append("!!!");
+        System.out.println(i.test(sb.toString()));
+    }
+}
