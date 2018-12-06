77e9bcd375515d222d7de381f22a8dc595b3cda4
BorisBNikolov <nikolov.b.boirs@gmail.com>
2018-12-06 14:06:47 +0200

    homework lamba

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -1,13 +1,14 @@
 package com.musala.javacourse181112.tasks;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Created by Iva Koleva on 03.12.2018
  */
 public class LambdaExercise {
-    public static void main(final String[] args) {
-        // TODO: implement
-        final Function<String, StringBuilder> stringToStringBuilderFunction;
-    }
+        public static void main(final String[] args) {
+            final Function<String,StringBuilder> castStringToStringBuilder = (string)->new StringBuilder(string);
+            final Predicate<String> stringLengthPredicate = (checkedString)-> checkedString.length()==10;
+        }
 }
