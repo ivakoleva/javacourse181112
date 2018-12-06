86cb1019066c5d5e84966bff36b829e3d771fb43
VermilionPride <dimitar.ang.mgu@gmail.com>
2018-12-06 16:44:48 +0200

    BiConsumer

---
+++ src/main/java/com/musala/javacourse181112/ForFun/BiConsumerExample.java
@@ -0,0 +1,12 @@
+package com.musala.javacourse181112.ForFun;
+
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+public class BiConsumerExample {
+    public static void main(String[] args) {
+        PipedInputStream inputStream = new PipedInputStream();
+        PipedOutputStream outputStream = new PipedOutputStream();
+
+    }
+}
