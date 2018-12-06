b4bcfb580fd72cd444ff64ffd34949d2b507c6e0
VermilionPride <dimitar.ang.mgu@gmail.com>
2018-12-06 18:08:00 +0200

    Lambda example full

--- src/main/java/com/musala/javacourse181112/ForFun/LambdaExample.java
+++ src/main/java/com/musala/javacourse181112/ForFun/LambdaExample.java
@@ -5,10 +5,18 @@ public class LambdaExample {
 
         System.out.println("=== RunnableTest ===");
 
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello world !");
+            }
+        };
+
         // Lambda Runnable
         Runnable r2 = () -> System.out.println("Hello world two!");
 
         // Run em!
+        r1.run();
         r2.run();
 
     }
