4167a70932a7013c31332659ba1cf04c7bb34482
dgluharov <dgluharov@gmail.com>
2018-12-06 11:35:34 +0200

    file exercise

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -1,14 +1,20 @@
 package com.musala.javacourse181112.tasks;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Created by Iva Koleva on 03.12.2018
  */
+
+/* Lambda exercise
+        *** implement String to StringBuilder function
+        *** implement String predicate - if string is length of 10*/
+
 public class LambdaExercise {
     private static final Object StringBuilder = new StringBuilder();
     public static void main(final String[] args) {
-        // TODO: implement
+
 
 
 
