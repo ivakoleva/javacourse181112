1dcc346795421c7018c7d86e662e6948404f4529
anton kolev <antongtkolev@gmail.com>
2018-12-06 12:47:57 +0200

    Lambda Exercise(String to StringBuilder,String predicate)

--- src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/LambdaExercise.java
@@ -2,12 +2,18 @@ package com.musala.javacourse181112.tasks;
 
 import java.util.function.Function;
 
+import java.util.function.Predicate;
+
 /**
  * Created by Iva Koleva on 03.12.2018
  */
 public class LambdaExercise {
     public static void main(final String[] args) {
-        // TODO: implement
+
         final Function<String, StringBuilder> stringToStringBuilderFunction;
+        stringToStringBuilderFunction = (s) -> new StringBuilder(s);
+
+        final Predicate<String> stringLengthOf10=(s)-> s!=null && s.length()==10;
+        System.out.println( stringLengthOf10.test("Hello Java"));
     }
 }
