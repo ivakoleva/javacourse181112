77a48a4a586f78da16114d7a5edaaba25f687088
Iva Koleva <iva.koleva@clearcode.bg>
2018-12-10 16:47:12 +0200

    homework review

--- src/main/java/com/musala/javacourse181112/tasks/CollectionsExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/CollectionsExercise.java
@@ -8,35 +8,33 @@ public class CollectionsExercise {
     private final static List<String> ENTRY_ELEMENTS = Arrays.asList("apple", "orange", "grapes", "cherry", "strawberry");
     private final static String[] KEY_VALUES = ENTRY_ELEMENTS.toArray(new String[0]);
 
-
     public static void main(final String[] args) {
-        testSortedSet(ENTRY_ELEMENTS);
-        testLinkedList(ENTRY_ELEMENTS);
-        testBlockingQueue(ENTRY_ELEMENTS);
-        testDeque(ENTRY_ELEMENTS);
-        testMap(KEY_VALUES);
+        testSortedSet();
+        testLinkedList();
+        testBlockingQueue();
+        testDeque();
+        testMap();
     }
 
-    private static void testSortedSet(final List<String> s){
-        SortedSet<String> stringTreeSet = new TreeSet<>(s);
+    private static void testSortedSet() {
+        final SortedSet<String> stringTreeSet = new TreeSet<>(ENTRY_ELEMENTS);
         System.out.println("Tree Set before loop: " + stringTreeSet);
-        for(String word : stringTreeSet){
+        for (String word : stringTreeSet) {
             System.out.println(word); // elements are automatically sorted
         }
-
     }
 
-    private static void testLinkedList(final List<String> s){
-        List<String> stringLinkedList = new LinkedList<>(s);
+    private static void testLinkedList() {
+        final List<String> stringLinkedList = new LinkedList<>(ENTRY_ELEMENTS);
         stringLinkedList.add(0, "mango");
         System.out.println("Linked List: " + stringLinkedList);
     }
 
-    private static void testBlockingQueue (final List<String> s){
-        BlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(20, true, s);
-        try{
-             arrayBlockingQueue.put("watermelon");
-             arrayBlockingQueue.take(); // retrieve and remove element from the head
+    private static void testBlockingQueue() {
+        final BlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(20, true, ENTRY_ELEMENTS);
+        try {
+            arrayBlockingQueue.put("watermelon");
+            arrayBlockingQueue.take(); // retrieve and remove element from the head
         } catch (InterruptedException e) {
             e.printStackTrace(); //  method will block if the queue is full, waiting for space to become available,
             // it will throw InterruptedException if the current thread is interrupted.
@@ -44,17 +42,17 @@ public class CollectionsExercise {
         System.out.println("ArrayBlockingQueue: " + arrayBlockingQueue);
     }
 
-    private static void testDeque(final List<String> s){
-        Deque<String> stringDeque = new ArrayDeque<>(s);
+    private static void testDeque() {
+        final Deque<String> stringDeque = new ArrayDeque<>(ENTRY_ELEMENTS);
         stringDeque.addFirst("melon");
         stringDeque.addLast("pear");
         System.out.println("Deque: " + stringDeque);
     }
 
-    private static void testMap(final String[] keyValue){
-        SortedMap<String, Integer> treeMap = new TreeMap<>();
-        for(int i = 0; i < keyValue.length; i++){
-            treeMap.put(keyValue[i], i);
+    private static void testMap() {
+        final SortedMap<String, Integer> treeMap = new TreeMap<>();
+        for (int i = 0; i < KEY_VALUES.length; i++) {
+            treeMap.put(KEY_VALUES[i], i);
         }
         System.out.println("Tree Map: " + treeMap);
         treeMap.entrySet().removeIf(k -> k.getKey().equals("apple"));
