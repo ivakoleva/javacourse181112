83b55bb9f32bb36d577590e4257fa036eb09a782
BorisBNikolov <nikolov.b.boirs@gmail.com>
2018-12-10 16:27:22 +0200

    homework, to be continue...

--- src/main/java/com/musala/javacourse181112/tasks/CollectionImplentations.java
+++ src/main/java/com/musala/javacourse181112/tasks/CollectionImplentations.java
@@ -10,65 +10,67 @@ public class CollectionImplentations {
         arrayList();
         linkedList();
         queue();
+        deque();
     }
-    public static void hashSet(){
-        HashSet<String> hashSet=new HashSet();
+
+    public static void hashSet() {
+        HashSet<String> hashSet = new HashSet();
         hashSet.add("One");
         hashSet.add("Two");
         hashSet.add("Three");
         hashSet.add("Four");
         hashSet.add("Five");
-        Iterator<String> i=hashSet.iterator();
-        System.out.println("Is the hashSet empty: "+ hashSet.isEmpty());
-        while(i.hasNext())
-        {
+        Iterator<String> i = hashSet.iterator();
+        System.out.println("Is the hashSet empty: " + hashSet.isEmpty());
+        while (i.hasNext()) {
             System.out.println(i.next());
         }
-        System.out.println("Is the hashSet contains five: "+ hashSet.contains("Five"));
-        System.out.println("The size of the hashSet: "+ hashSet.size());
+        System.out.println("Is the hashSet contains five: " + hashSet.contains("Five"));
+        System.out.println("The size of the hashSet: " + hashSet.size());
         hashSet.remove("Five");
-        System.out.println("The size of the hashSet after removing five: "+ hashSet.size());
+        System.out.println("The size of the hashSet after removing five: " + hashSet.size());
         hashSet.clear();
     }
-    public static void treeSet(){
-        TreeSet<String> treeSet=new TreeSet();
+
+    public static void treeSet() {
+        TreeSet<String> treeSet = new TreeSet();
         treeSet.add("One");
         treeSet.add("Two");
         treeSet.add("Three");
         treeSet.add("Four");
         treeSet.add("Five");
-        Iterator<String> i=treeSet.iterator();
-        System.out.println("Is the treeSet empty: "+ treeSet.isEmpty());
-        while(i.hasNext())
-        {
+        Iterator<String> i = treeSet.iterator();
+        System.out.println("Is the treeSet empty: " + treeSet.isEmpty());
+        while (i.hasNext()) {
             System.out.println(i.next());
         }
-        System.out.println("Is the treeSet contains five: "+ treeSet.contains("Five"));
-        System.out.println("The size of the treeSet: "+ treeSet.size());
-        System.out.println("Polling the last element: "+ treeSet.pollLast());
-        System.out.println("The size of the treeSet after polling the last element: "+ treeSet.size());
+        System.out.println("Is the treeSet contains five: " + treeSet.contains("Five"));
+        System.out.println("The size of the treeSet: " + treeSet.size());
+        System.out.println("Polling the last element: " + treeSet.pollLast());
+        System.out.println("The size of the treeSet after polling the last element: " + treeSet.size());
         treeSet.clear();
 
     }
-    public static void linkedHashSet(){
-        LinkedHashSet<String> linkedHashSet=new LinkedHashSet();
+
+    public static void linkedHashSet() {
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet();
         linkedHashSet.add("One");
         linkedHashSet.add("Two");
         linkedHashSet.add("Three");
         linkedHashSet.add("Four");
         linkedHashSet.add("Five");
-        Iterator<String> i=linkedHashSet.iterator();
-        System.out.println("Is the linkedHashSet empty: "+ linkedHashSet.isEmpty());
-        while(i.hasNext())
-        {
+        Iterator<String> i = linkedHashSet.iterator();
+        System.out.println("Is the linkedHashSet empty: " + linkedHashSet.isEmpty());
+        while (i.hasNext()) {
             System.out.println(i.next());
         }
-        System.out.println("Is the linkedHashSet contains five: "+ linkedHashSet.contains("Five"));
-        System.out.println("The size of the linkedHashSet: "+ linkedHashSet.size());
+        System.out.println("Is the linkedHashSet contains five: " + linkedHashSet.contains("Five"));
+        System.out.println("The size of the linkedHashSet: " + linkedHashSet.size());
         linkedHashSet.remove("Five");
-        System.out.println("The size of the linkedHashSet after removing five: "+ linkedHashSet.size());
+        System.out.println("The size of the linkedHashSet after removing five: " + linkedHashSet.size());
         linkedHashSet.clear();
     }
+
     public static void arrayList() {
         ArrayList<String> arrayList = new ArrayList();
         arrayList.add("One");
@@ -85,7 +87,7 @@ public class CollectionImplentations {
         arrayList.clear();
     }
 
-    public static void linkedList(){
+    public static void linkedList() {
         LinkedList<String> linkedList = new LinkedList();
         linkedList.add("One");
         linkedList.add("Two");
@@ -102,14 +104,15 @@ public class CollectionImplentations {
         System.out.println("The size is: " + linkedList.size());
         linkedList.clear();
     }
-    public static void queue(){
+
+    public static void queue() {
         PriorityQueue<String> queue = new PriorityQueue();
         queue.add("One");
         queue.add("Two");
         queue.add("Three");
         queue.add("Four");
         queue.add("Five");
-        queue.forEach(i->System.out.print(i+ " "));
+        queue.forEach(i -> System.out.print(i + " "));
         System.out.println("Is the queue empty: " + queue.isEmpty());
         System.out.println("Peek (first element): " + queue.peek());
         System.out.println("The size is: " + queue.size());
@@ -118,5 +121,20 @@ public class CollectionImplentations {
         queue.clear();
     }
 
-}
+    public static void deque() {
+        ArrayDeque<String> deque = new ArrayDeque();
+        deque.add("One");
+        deque.add("Two");
+        deque.add("Three");
+        deque.add("Four");
+        deque.add("Five");
+        deque.forEach(i -> System.out.print(i + " "));
+        System.out.println("Is the deque empty: " + deque.isEmpty());
+        System.out.println("Peek (first element): " + deque.peek());
+        System.out.println("The size is: " + deque.size());
+        System.out.println("The poll (remove the first element and peek it): " + deque.poll());
+        System.out.println("The size is: " + deque.size());
+        deque.clear();
+    }
 
+}
