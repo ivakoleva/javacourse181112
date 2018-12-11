c873c82cd4ff1919a301875843aff6b307f817c0
IcouldEsmileof <aykut@mail.bg>
2018-12-11 19:25:14 +0200

    homework

--- src/main/java/com/musala/javacourse181112/tasks/BlockingQueueExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/BlockingQueueExercise.java
@@ -7,16 +7,16 @@ import java.util.concurrent.BlockingQueue;
 
 public class BlockingQueueExercise {
     public static void main(String[] args) {
-        final BlockingQueue<Integer> blockingQueue=new ArrayBlockingQueue<>(10);
-        for(int i=0;i<10;i++){
+        final BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(10);
+        for (int i = 0; i < 10; i++) {
             blockingQueue.offer(i);
         }
         final ArrayQueue<Integer> queue = new ArrayQueue<Integer>(5);
-        blockingQueue.drainTo(queue,5);
+        blockingQueue.drainTo(queue, 5);
         System.out.println("BlockingQueue:");
-        blockingQueue.forEach(i-> System.out.print(i+" "));
-        System.out.println(System.lineSeparator()+"ArrayQueue:");
-        queue.forEach(i-> System.out.print(i+" "));
+        blockingQueue.forEach(i -> System.out.print(i + " "));
+        System.out.println(System.lineSeparator() + "ArrayQueue:");
+        queue.forEach(i -> System.out.print(i + " "));
         System.out.println();
     }
 }
