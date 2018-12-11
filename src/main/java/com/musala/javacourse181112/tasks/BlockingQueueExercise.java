618106e06f15165610d654b8e8114e587c2211fa
YordanRadkov <iordanradkov@gmail.com>
2018-12-11 19:17:14 +0200

    BlockingQueue Exercise

---
+++ src/main/java/com/musala/javacourse181112/tasks/BlockingQueueExercise.java
@@ -0,0 +1,21 @@
+package com.musala.javacourse181112.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueExercise {
+    public static void main(String[] args) {
+        final BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue(10);
+       for (int i = 0; i < 10; i++){
+           blockingQueue.offer(i);
+       }
+
+        final List<Integer> integerList = new ArrayList<>();
+         blockingQueue.drainTo(integerList,5);
+        System.out.println(integerList);
+
+
+    }
+}
