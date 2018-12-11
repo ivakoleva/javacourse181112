aef79ee8b42f86249f0bc900b23b1ec0b0c18cc3
Iva Koleva <iva.koleva@clearcode.bg>
2018-12-11 17:50:23 +0200

    exercise review

--- src/main/java/com/musala/javacourse181112/tasks/collections/DeduplicationExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/collections/DeduplicationExercise.java
@@ -7,38 +7,33 @@ import java.util.stream.IntStream;
 
 public class DeduplicationExercise {
     public static void main(final String[] args) {
-        final List<Integer> integerList = IntStream.range(0,10).boxed().collect(Collectors.toCollection(ArrayList::new));
+        final List<Integer> integerList = IntStream.range(0, 10).boxed().collect(Collectors.toCollection(ArrayList::new));
         integerList.add(1);
         integerList.add(2);
+        integerList.add(1);
+        integerList.add(1);
         integerList.add(3);
         integerList.add(0);
+        integerList.add(1);
         integerList.add(50);
         integerList.add(10);
+        integerList.add(1);
         integerList.add(1);
 
         System.out.println("The list before processing:");
         System.out.println(integerList);
 
         System.out.println("The list after processing");
-        System.out.println(doDeduplicateWithEquals(integerList));
+        System.out.println(deduplicate(integerList));
         //System.out.println(doDeduplicateWithHashCode(integerList));
     }
 
-    private static List doDeduplicateWithEquals(final List<Integer> integerList){
-        for (int i =0; i < integerList.size(); i++) {
+    // TODO: modify while loop - using iterator
+    // TODO: using functional interface
+    private static List deduplicate(final List<Integer> integerList) {
+        for (int i = 0; i < integerList.size(); i++) {
             for (int j = i + 1; j < integerList.size(); j++) {
-                if (integerList.get(i).equals(integerList.get(j))){
-                    integerList.remove(j);
-                }
-            }
-        }
-        return integerList;
-    }
-
-    private static List doDeduplicateWithHashCode(final List<Integer> integerList){
-        for (int i =0; i < integerList.size(); i++) {
-            for (int j = i + 1; j < integerList.size(); j++) {
-                if (integerList.get(i).hashCode() == integerList.get(j).hashCode()){
+                if (integerList.get(i).equals(integerList.get(j))) {
                     integerList.remove(j);
                 }
             }
