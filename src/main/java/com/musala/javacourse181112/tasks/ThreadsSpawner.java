9c0928202340daef1155e36ff2eda32c0f2d0d47
Iva Koleva <iva.koleva@clearcode.bg>
2018-12-11 16:39:45 +0200

    exercise review

--- src/main/java/com/musala/javacourse181112/tasks/ThreadsSpawner.java
+++ src/main/java/com/musala/javacourse181112/tasks/ThreadsSpawner.java
@@ -4,50 +4,44 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
+// ok
 public class ThreadsSpawner {
     private final static int LIMIT = 10;
     private static Map<Long, String> LONG_STRING_MAP = Collections.synchronizedSortedMap(new TreeMap<>());
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         LONG_STRING_MAP = Collections.checkedMap(new TreeMap<>(), Long.class, String.class);
 
-    /*final BiConsumer<Long, String> printToStdout = (key, message) ->
+        /*final BiConsumer<Long, String> printToStdout = (key, message) ->
             System.out.println(Thread.currentThread().getName() + " (" + key + "): " + message);*/
         final Thread producerThread = new Thread(() -> {
-
+            final String value = "producer";
             while (!Thread.interrupted()) {
-                final String value = "producer";
-                final long millis = System.currentTimeMillis();
-
-
                 try {
-                    LONG_STRING_MAP.put(millis, value);
+                    LONG_STRING_MAP.put(System.currentTimeMillis(), value);
                     Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ignored) {
+                    //e.printStackTrace();
+                    System.out.println("producerThread interrupted.");
                 }
             }
-
         });
 
-
         final Thread consumerThread = new Thread(() -> {
-            final String value = "consumer";
+            //final String value = "consumer";
             int i = 0;
             while (!Thread.interrupted() && i < LIMIT) {
                 if (LONG_STRING_MAP.isEmpty()) {
                     continue;
                 }
-                for (Map.Entry<Long, String> pair : LONG_STRING_MAP.entrySet()) {
-                    System.out.println("key: " + pair.getKey() + "value: " + pair.getValue());
+                for (Map.Entry<Long, String> entry : LONG_STRING_MAP.entrySet()) {
+                    System.out.println("key: " + entry.getKey() + "value: " + entry.getValue());
                 }
                 i++;
                 System.out.println("Print LONG_STRING_MAP.size()" + LONG_STRING_MAP.size());
             }
             producerThread.interrupt();
-
         });
-
 
         producerThread.start();
         consumerThread.start();
@@ -60,8 +54,6 @@ public class ThreadsSpawner {
 
         System.exit(0);
     }
-
-
 }
 
 
