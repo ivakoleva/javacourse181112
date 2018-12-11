d1452b6f70e532c39326fa5c7381fe17b6bbc6e4
YordanRadkov <iordanradkov@gmail.com>
2018-12-11 19:19:37 +0200

    Deduplication Exercise

--- src/main/java/com/musala/javacourse181112/tasks/BlockingQueueExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/BlockingQueueExercise.java
@@ -6,16 +6,14 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class BlockingQueueExercise {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         final BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue(10);
-       for (int i = 0; i < 10; i++){
-           blockingQueue.offer(i);
-       }
+        for (int i = 0; i < 10; i++) {
+            blockingQueue.offer(i);
+        }
 
         final List<Integer> integerList = new ArrayList<>();
-         blockingQueue.drainTo(integerList,5);
+        blockingQueue.drainTo(integerList, 5);
         System.out.println(integerList);
-
-
     }
 }
