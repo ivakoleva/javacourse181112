d08831cd0c739b8b76c308de37e2445172f6468f
dgluharov <dgluharov@gmail.com>
2018-12-11 19:32:54 +0200

    BlockingQueueExercise

--- src/main/java/com/musala/javacourse181112/tasks/Collections/BlockingQueueExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/Collections/BlockingQueueExercise.java
@@ -1,17 +1,24 @@
 package com.musala.javacourse181112.tasks.Collections;
 
+import com.sun.jmx.remote.internal.ArrayQueue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 //** BlockingQueue drainTo Exercise
 //        *** instantiate a collection, then populate with 5 of 10 queue elements
 public class BlockingQueueExercise {
-    public static void main(String[] args) throws Exception {
-        final BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(10);
-        for (int i = 0;i<1-; i++){
-            blockingQueue
+    public static void main(String[] args) {
+        final BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(10);
+        for (int i = 0; i < 10; i++) {
+            blockingQueue.offer(i);
         }
-
+        final ArrayQueue<Integer> queue = new ArrayQueue<Integer>(5);
+        blockingQueue.drainTo(queue, 5);
+        System.out.println("BlockingQueue: ");
+        blockingQueue.forEach(i -> System.out.println(i + " "));
+        System.out.println(System.lineSeparator() + "ArrayQueue: ");
+        queue.forEach(i -> System.out.println(i + " "));
+        System.out.println();
 
 
     }
