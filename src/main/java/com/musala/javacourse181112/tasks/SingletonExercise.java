e912ce41cdc363ce991ad9d77eb066508d2a008b
BorisBNikolov <nikolov.b.boirs@gmail.com>
2018-12-11 16:33:11 +0200

    SingletonExercise

--- src/main/java/com/musala/javacourse181112/tasks/SingletonExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/SingletonExercise.java
@@ -1,4 +1,21 @@
 package com.musala.javacourse181112.tasks;
 
 public class SingletonExercise {
+    public static void main(final String[] args) {
+        final SingletonApplicationClass singletonApplication = SingletonApplicationClass.getInstance();
+        final SingletonApplicationClass singletonApplication1 = SingletonApplicationClass.getInstance();
+        assert singletonApplication == singletonApplication1;
+    }
 }
+
+class SingletonApplicationClass {
+    private static final SingletonApplicationClass INSTANCE = new SingletonApplicationClass();
+
+    private SingletonApplicationClass() {
+    }
+
+    public static SingletonApplicationClass getInstance() {
+        return INSTANCE;
+    }
+}
+
