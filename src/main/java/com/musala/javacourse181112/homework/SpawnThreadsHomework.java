a52bd1ab6591fb1a114c040cbf204afd3244fc3f
cecoweee <newcecoweee@gamil.com>
2018-12-13 19:01:53 +0200

    Spawning Threads Problems

--- src/main/java/com/musala/javacourse181112/homework/SpawnThreadsHomework.java
+++ src/main/java/com/musala/javacourse181112/homework/SpawnThreadsHomework.java
@@ -7,20 +7,20 @@ import java.util.Objects;
 import java.util.function.BiConsumer;
 
 public class SpawnThreadsHomework {
-    public static Map<Long, String> LONG_STRING_HASHMAP = Collections.synchronizedMap(new HashMap<>());
+    public static Map<Long, String> LONG_STRING_MAP = Collections.synchronizedMap(new HashMap<>());
     public static final int MAP_LENGTH = 10;
 
     public static void main(String[] args) {
-        LONG_STRING_HASHMAP = Collections.checkedMap(new HashMap<>(), Long.class, String.class);
+        LONG_STRING_MAP = Collections.checkedMap(new HashMap<>(), Long.class, String.class);
 
         final BiConsumer<Long, String> populateMap = (key, value) ->
-                LONG_STRING_HASHMAP.put(key, value);
+                LONG_STRING_MAP.put(key, value);
 
         final Thread producerThread = new Thread(() -> {
 
             while (!Thread.interrupted()) {
                 final long millis = System.currentTimeMillis();
-                if (LONG_STRING_HASHMAP.isEmpty()) {
+                if (LONG_STRING_MAP.isEmpty()) {
                     continue;
                 }
                 populateMap.accept(System.currentTimeMillis(), "Random String: " + Objects.toString(millis, null));
@@ -37,14 +37,14 @@ public class SpawnThreadsHomework {
             int i = 0;
 
             while (!Thread.interrupted() && i++ < MAP_LENGTH) {
-                if (LONG_STRING_HASHMAP.isEmpty()) {
+                if (LONG_STRING_MAP.isEmpty()) {
                     continue;
                 }
-                for (Map.Entry<Long, String> pair : LONG_STRING_HASHMAP.entrySet()) {
+                for (Map.Entry<Long, String> pair : LONG_STRING_MAP.entrySet()) {
                     System.out.println("Key: " + pair.getKey() + "Value: " + pair.getValue());
                 }
                 i++;
-                System.out.println(LONG_STRING_HASHMAP.size());
+                System.out.println(LONG_STRING_MAP.size());
 
             }
             producerThread.interrupt();
@@ -62,5 +62,6 @@ public class SpawnThreadsHomework {
             e.printStackTrace();
         }
         System.exit(0);
+        // 2nd Commit
     }
 }
