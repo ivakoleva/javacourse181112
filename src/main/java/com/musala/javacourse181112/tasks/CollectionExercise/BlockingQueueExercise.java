53fce70d220d317bf31c345b89ae46e3c215eecc
Blueshadows <blueshadows98@gmail.com>
2018-12-13 12:28:32 +0200

    update

---
+++ src/main/java/com/musala/javacourse181112/tasks/CollectionExercise/BlockingQueueExercise.java
@@ -0,0 +1,11 @@
+package com.musala.javacourse181112.tasks.CollectionExercise;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueExercise {
+    public static void main(final String[] args) {
+        BlockingQueue<String> arrayBlockingQueue =new ArrayBlockingQueue<>(10);
+
+    }
+}
---
+++ src/main/java/com/musala/javacourse181112/tasks/CollectionExercise/CollectionImplementation.java
@@ -0,0 +1,28 @@
+package com.musala.javacourse181112.tasks.CollectionExercise;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionImplementation {
+
+    public static void main(final String[] args) {
+
+
+        implementUsingHashSet();
+
+
+
+
+    }
+    private static void implementUsingHashSet(){
+          final Set<String> hashSet=new HashSet<>();
+        System.out.println("HasSet Implementation");
+
+
+
+
+    }
+
+
+
+}
