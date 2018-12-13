22e5a7819770cd1696808e8226160c4cc4a0a2d6
IcouldEsmileof <aykut@mail.bg>
2018-12-13 18:13:19 +0200

    homework

--- src/main/java/com/musala/javacourse181112/tasks/MapExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/MapExercise.java
@@ -9,8 +9,17 @@ public class MapExercise {
         final Map<Character, Integer> charToIntMap = new HashMap<>();
         for (char c = 24; c < 165; c += 3) {
             charToIntMap.put(c, (int) c);
+            charToIntMap.forEach((k,v)-> System.out.print(k+"->"+v));
+            System.out.println();
         }
-
+        for(Character c:charToIntMap.keySet()){
+            System.out.print(c+" ");
+        }
+        System.out.println();
+        for(Integer i:charToIntMap.values()){
+            System.out.print(i+" ");
+        }
+        System.out.println();
         // could be one-liner lambda
         final Iterator iteratorEntrySet = charToIntMap.entrySet().iterator();
         for (; iteratorEntrySet.hasNext(); ) {
