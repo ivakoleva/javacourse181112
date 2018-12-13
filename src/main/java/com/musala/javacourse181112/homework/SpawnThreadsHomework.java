4194466983ddc16f23b15df6d90ac86dc9c37a7f
Iva Koleva <iva.koleva@clearcode.bg>
2018-12-13 19:55:02 +0200

    homework review

--- src/main/java/com/musala/javacourse181112/homework/SpawnThreadsHomework.java
+++ src/main/java/com/musala/javacourse181112/homework/SpawnThreadsHomework.java
@@ -1,29 +1,26 @@
 package com.musala.javacourse181112.homework;
 
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 
 public class SpawnThreadsHomework {
-    public static Map<Long, String> LONG_STRING_MAP = Collections.synchronizedMap(new HashMap<>());
-    public static final int MAP_LENGTH = 10;
+    public final static Map<Long, String> LONG_STRING_MAP = Collections.checkedMap(
+            new ConcurrentHashMap<>(), Long.class, String.class); //Collections.synchronizedMap(new HashMap<>());
+    public static final int MAX_PRODUCED_ENTRIES_COUNT = 10;
+
+    private static final Object monitor = new Object();
 
     public static void main(String[] args) {
-        LONG_STRING_MAP = Collections.checkedMap(new HashMap<>(), Long.class, String.class);
-
-        final BiConsumer<Long, String> populateMap = (key, value) ->
-                LONG_STRING_MAP.put(key, value);
+        // demo BiConsumer show-case purpose
+        final BiConsumer<Long, String> populateMap = LONG_STRING_MAP::put;
 
         final Thread producerThread = new Thread(() -> {
-
             while (!Thread.interrupted()) {
                 final long millis = System.currentTimeMillis();
-                if (LONG_STRING_MAP.isEmpty()) {
-                    continue;
-                }
-                populateMap.accept(System.currentTimeMillis(), "Random String: " + Objects.toString(millis, null));
+                populateMap.accept(System.currentTimeMillis(), "Random String: " + millis);
 
                 try {
                     Thread.sleep(1000 * 3);
@@ -32,28 +29,29 @@ public class SpawnThreadsHomework {
                 }
             }
         });
-
         final Thread consumerThread = new Thread(() -> {
             int i = 0;
 
-            while (!Thread.interrupted() && i++ < MAP_LENGTH) {
-                if (LONG_STRING_MAP.isEmpty()) {
-                    continue;
-                }
-                for (Map.Entry<Long, String> pair : LONG_STRING_MAP.entrySet()) {
-                    System.out.println("Key: " + pair.getKey() + "Value: " + pair.getValue());
-                }
-                i++;
-                System.out.println(LONG_STRING_MAP.size());
+            while (!Thread.interrupted() && i < MAX_PRODUCED_ENTRIES_COUNT) {
+                synchronized (monitor) {
+                    final Iterator<Map.Entry<Long, String>> iterator = LONG_STRING_MAP.entrySet().iterator();
+                    if (!iterator.hasNext()) {
+                        continue;
+                    }
 
+                    final Map.Entry<Long, String> entry = iterator.next();
+                    System.out.println("Key: " + entry.getKey() + "Value: " + entry.getValue());
+                    iterator.remove();
+                    i++;
+
+                    System.out.println(LONG_STRING_MAP.size());
+                }
             }
             producerThread.interrupt();
-
-
         });
+
         producerThread.start();
         consumerThread.start();
-
 
         try {
             producerThread.join();
@@ -61,7 +59,7 @@ public class SpawnThreadsHomework {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         System.exit(0);
-        // 2nd Commit
     }
 }
