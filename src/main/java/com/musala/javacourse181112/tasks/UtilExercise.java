2b6719527aa3495a34e2b304f82ad34ca4acb49e
Galq <galina.sansarowa@gmail.com>
2018-12-13 18:58:46 +0200

    Util Exercise

--- src/main/java/com/musala/javacourse181112/tasks/UtilExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/UtilExercise.java
@@ -7,21 +7,22 @@ public class UtilExercise {
     public static void main(String[] args) {
        final List<Integer> integerlist=new ArrayList<>();
         IntStream.range(0, 10).forEach(integerlist::add);
+        System.out.println("Array:"+ integerlist);
         Collections.shuffle(integerlist);
-        System.out.println(integerlist);
+        System.out.println("Shuffled Array:"+ integerlist);
         Collections.sort(integerlist);
-        System.out.println(integerlist);
+        System.out.println( "Sorted Array:"+ integerlist);
         Collections.reverse(integerlist);
-        System.out.println(integerlist);
+        System.out.println( "Reverset Array:" + integerlist);
         List<Integer> secondList=new ArrayList<>();
         for (int i=0;i<=100;i++){
             secondList.add(1);
 
         }
-        System.out.println(secondList);
+        System.out.println("Second List" + secondList);
         final Set <Integer> set= new HashSet<>(integerlist);
         set.addAll(secondList);
-        System.out.println(set);
+        System.out.println("Concatenate Arrays:"  + set);
 
     }
 }
