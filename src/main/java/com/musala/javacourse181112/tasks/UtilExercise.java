39390b057b7b8f91f2a480a886303e258b3fb035
anton kolev <antongtkolev@gmail.com>
2018-12-13 19:19:05 +0200

    UtilExercise

--- src/main/java/com/musala/javacourse181112/tasks/UtilExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/UtilExercise.java
@@ -3,6 +3,7 @@ package com.musala.javacourse181112.tasks;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Collections;
 
 public class UtilExercise {
     public static void main(String[] args) {
@@ -24,6 +25,26 @@ public class UtilExercise {
 
             System.out.println(iter.next());
         }
+        Collections.shuffle(integerList);
+
+        integerList.forEach(i-> System.out.println(i));
+
+        Collections.sort(integerList);
+
+        integerList.forEach(i-> System.out.println(i));
+
+        Collections.reverse(integerList);
+
+        integerList.forEach(i-> System.out.println(i));
+
+      final List<Integer> integerList1 =Collections.nCopies(100,1);
+
+
+
+
+
+
+
 
 
 
