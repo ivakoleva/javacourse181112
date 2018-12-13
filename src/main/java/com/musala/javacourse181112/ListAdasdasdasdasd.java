d8fcc473d9d352b6960d91ba144081f44f02a769
Iva Koleva <iva.koleva@clearcode.bg>
2018-12-13 17:36:15 +0200

    exercise review

--- src/main/java/com/musala/javacourse181112/ListAdasdasdasdasd.java
+++ src/main/java/com/musala/javacourse181112/ListAdasdasdasdasd.java
@@ -1,18 +1,22 @@
 package com.musala.javacourse181112;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ListAdasdasdasdasd {
     public static void main(String[] args) {
-        List<Integer> integerList = new ArrayList<>(120);
-        integerList.set(100,100);
-        System.out.println(integerList.get(integerList.get(100)));
+        final List<Integer> integerList = new ArrayList<>(120);
+        IntStream.range(0, 100).forEach(i -> integerList.add(null));
+        integerList.add(100);
+
+        //integerList.set(100, 100);
+        /*System.out.println(integerList.get(integerList.get(100)));
         System.out.println(integerList.size());
+
         List<Integer> integerLinkedList = new LinkedList<>();
-        integerLinkedList.set(100,200);
+        integerLinkedList.set(100, 200);
         System.out.println(integerLinkedList.get(100));
-        System.out.println(integerLinkedList.size());
+        System.out.println(integerLinkedList.size());*/
     }
 }
