810dae664a0a0f0ead72fb726b19e6c036c57b1c
Mirella Krysteva <mirella.krysteva@gmail.com>
2018-12-17 20:17:35 +0200

    GenericTriDimensionalMatrixExercise added

--- src/main/java/com/musala/javacourse181112/tasks/GenericsTriDimensionalMatrixExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/GenericsTriDimensionalMatrixExercise.java
@@ -1,5 +1,8 @@
 package com.musala.javacourse181112.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Iva Koleva on 17.12.2018
  */
@@ -13,6 +16,22 @@ public class GenericsTriDimensionalMatrixExercise {
                         }
                 };
 
-        // TODO: using List, applying generics
+        System.out.println(triDimensionalList(1, 4, 3));
+    }
+
+    public static List<List<List<Integer>>> triDimensionalList(int dimension1, int dimension2, int dimension3) {
+        final List<List<List<Integer>>> triDimensionalListLevel1 = new ArrayList<>(dimension1);
+        for (int i = 0; i < dimension1; i++) {
+            List<List<Integer>> triDimensionalListLevel2 = new ArrayList<>(dimension2);
+            for (int j = 0; j < dimension2; j++) {
+                List<Integer> triDimensionalListLevel3 = new ArrayList<>(dimension3);
+                for (int k = 6; k < dimension3 + 6; k++) {
+                    triDimensionalListLevel3.add(k);
+                }
+                triDimensionalListLevel2.add(triDimensionalListLevel3);
+            }
+            triDimensionalListLevel1.add(triDimensionalListLevel2);
+        }
+        return triDimensionalListLevel1;
     }
 }
