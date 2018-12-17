766299762283c25c54610d3fffbf902be73a9a6f
dgluharov <dgluharov@gmail.com>
2018-12-17 18:53:35 +0200

    Generics Tri Dimensional Matrix ExerciseG

---
+++ src/main/java/com/musala/javacourse181112/tasks/GnericsTriDimensionalMatrixExercise.java
@@ -0,0 +1,12 @@
+package com.musala.javacourse181112.tasks;
+
+public class GnericsTriDimensionalMatrixExercise {
+    public static void main(String[] args) {
+        final int[][][] intArray = {
+                {
+                        {1, 2, 3},
+                        {1, 2, 4}
+                }
+        };
+    }
+}
