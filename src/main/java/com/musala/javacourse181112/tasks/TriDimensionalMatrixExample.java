0e966ca025de82598c366efcb38ecc5719a1a1cf
cecoweee <newcecoweee@gamil.com>
2018-12-17 18:44:50 +0200

    Magics

---
+++ src/main/java/com/musala/javacourse181112/tasks/TriDimensionalMatrixExample.java
@@ -0,0 +1,20 @@
+package com.musala.javacourse181112.tasks;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TriDimensionalMatrixExample {
+    public static void main(final String[] args) {
+        final int[][][] intArray =
+                {
+                        {
+                                {1, 2, 3},
+                                {1, 2, 3}
+                        }
+                };
+        List<List<List<Integer>>> listListListInteger = new LinkedList<>();
+
+
+        // TODO: using List, applying generics
+    }
+}
