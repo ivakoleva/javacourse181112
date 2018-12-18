4be87f7eb475cee1859828a45bd622c478a7d460
dgluharov <dgluharov@gmail.com>
2018-12-18 14:51:52 +0200

    Homework GenericSet

--- src/main/java/com/musala/javacourse181112/tasks/Generics/GenericSet.java
+++ src/main/java/com/musala/javacourse181112/tasks/Generics/GenericSet.java
@@ -1,7 +1,18 @@
 package com.musala.javacourse181112.tasks.Generics;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * Create and populate a generic Set, that supports addition of at least Integer, Long, Short, Double
  */
 public class GenericSet {
-
+    public static void main(String[] args) {
+        final Set<Number> genericSet = new HashSet<>();
+        genericSet.add(7);
+        genericSet.add(12L);
+        genericSet.add((short)3);
+        genericSet.add(17.3D);
+        System.out.println(genericSet);
+    }
 }
