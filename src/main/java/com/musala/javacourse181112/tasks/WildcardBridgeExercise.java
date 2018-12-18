940e82f94e1fe6c13ddd8e1162f42d1f1d59115d
BorisBNikolov <nikolov.b.boirs@gmail.com>
2018-12-18 16:14:30 +0200

    WildCard Homework

--- src/main/java/com/musala/javacourse181112/tasks/WildcardBridgeExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/WildcardBridgeExercise.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class WildcardBridgeExercise {
     public static void main(String[] args) {
         List<?> wildCard = new ArrayList<>();
-        addAndPrintTheUnknown((List<Integer>)wildCard);
+        addAndPrintInteger((List<Integer>)wildCard);
     }
-    public static void addAndPrintTheUnknown(List<Integer> wildList) {
+    public static void addAndPrintInteger(List<Integer> wildList) {
         wildList.add(1);
         System.out.println(wildList.get(0));
     }
