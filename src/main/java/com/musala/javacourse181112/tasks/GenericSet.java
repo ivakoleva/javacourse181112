ee394616bf077550146bc9b00d622eca65f02599
Iva Koleva <iva.koleva@clearcode.bg>
2018-12-18 16:33:53 +0200

    homework review

--- src/main/java/com/musala/javacourse181112/tasks/GenericSet.java
+++ src/main/java/com/musala/javacourse181112/tasks/GenericSet.java
@@ -1,11 +1,13 @@
 package com.musala.javacourse181112.tasks;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GenericSet {
     private final static Set<?> GENERIC_SET = new HashSet<>();
     private final static Set<Number> NUMBER_SET = new HashSet<>();
 
+    //@SuppressWarnings("unchecked")
     public static void main(String[] args) {
         NUMBER_SET.add(2);
         NUMBER_SET.add(3.14);
@@ -13,13 +15,12 @@ public class GenericSet {
         NUMBER_SET.add(3L);
         System.out.println(NUMBER_SET);
 
-        copy(GENERIC_SET, NUMBER_SET);
-
+        final Set<?> set1 = copy(GENERIC_SET, NUMBER_SET);
+        System.out.println(set1);
     }
 
-    private static <T> Set<T> copy (Set<? extends Object> source, Set<? extends Number> destination){
-        return new HashSet<>();
+    private static <T> Set<T> copy(final Set<T> source, final Set<? extends Number> destination) {
+        destination.forEach(n -> source.add((T) n));
+        return source;
     }
-
-
 }
