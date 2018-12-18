e153f90beef88accc0852c571ddbaae8be5fe861
stefan.atanasov <stefanatanasov99@abv.bg>
2018-12-18 15:00:55 +0200

    Merge remote-tracking branch 'origin/master' into samplerunner
    # Conflicts:
    #	src/main/java/com/musala/javacourse181112/Hi.java

--- src/main/java/com/musala/javacourse181112/Hi.java
+++ src/main/java/com/musala/javacourse181112/Hi.java
@@ -1,14 +1,85 @@
 package com.musala.javacourse181112;
 
+import javax.management.StringValueExp;
+
 /**
  * Created by Iva Koleva, ClearCode Ltd. on 12.11.2018,
  * a significant bit of leva.bg project.
  */
+<<<<<<< HEAD
 public class Hi {
     public static void main(String[] args) {
         System.out.println("Hi!"); // hardcoded
         System.out.println(args[0]); // runtime
         //System.gc();
         System.exit(0);
+=======
+public class HelloWorld {
+    static final String CONSTANT_VALUE = "asd";
+    public static void defineAndInitializePrimitives (String inputValue)
+        System.out.println("Hi!");
+>>>>>>> primitive data samples
     }
+
+    {
+        System.out.println("Hi"); // hardcoded
+        System.out.println(args[0]); // runtime
+
+        System.gc;
+
+
+        System.exit;
+
+
+
+    public static void main1(StringValue) {
+        String Value1// local variable
+        String myVariable = "asd";
+        myVariable = "asd1";
+
+
+        byte myByteVariable = (byte) (123 + 1);
+        short myShortVariable = 1000;
+
+        int myIntVariable = (int) 1;
+        long myLongVariable = 1L;
+        float myFloatVariable = 1.0f;
+        double myDoubleVariable = 1.0
+        System.out.println("Hi!");
+ boolean myBooleanVariable = true; / false; // can only  be true or false}
+
+
+
+    char myCharVariable = 'a';
+    char a =  (char) 97;
+    char a1= "\0061':
+// numeric systems
+ int decimalInt = 100;  // decimal numeric system
+ int octalInt = 0144; // octal numeric system
+ int hexInt = 0X64; // hexadecimal
+ int  binaryInt =  ; // binary numeric systme
 }
+
+
+public static void defineAndInitializePrimitives (String inputValue) {
+String value1; // local variable
+
+final String myVariable = "asd\ "
+
+
+
+/*
+this is how you write multi line comment
+*/
+
+// scientific notation of numbers
+
+
+ double scientificInt = 1e2
+
+
+
+
+{
+public static void main (String [] args ) {
+    define
