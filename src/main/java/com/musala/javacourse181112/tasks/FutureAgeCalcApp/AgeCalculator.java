927d4179bb0ffb58933d4636baa4a6cb1a6c9606
anton kolev <antongtkolev@gmail.com>
2019-01-03 18:04:58 +0200

    Age Calculator

--- src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalcApp/AgeCalculator.java
+++ src/main/java/com/musala/javacourse181112/tasks/FutureAgeCalcApp/AgeCalculator.java
@@ -1,4 +1,16 @@
 package com.musala.javacourse181112.tasks.FutureAgeCalcApp;
 
 public class AgeCalculator {
+    private Person person;
+    private int yearsToadd;
+
+    public  AgeCalculator(Person person, int addAge){
+        this.person = person;
+        this.yearsToadd = addAge;
+    }
+    public int getFutureAge(){
+        return person.getAge() + yearsToadd;
+
+
+    }
 }
