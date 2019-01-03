390648475890c1325dc9901b9e7ff1361ebcc08d
Mirella Krysteva <mirella.krysteva@gmail.com>
2019-01-03 14:33:53 +0200

    StaticNonStaticMethodsExercise creation

---
+++ src/main/java/com/musala/javacourse181112/tasks/methodsExercise/Person.java
@@ -0,0 +1,4 @@
+package com.musala.javacourse181112.tasks.methodsExercise;
+
+public class Person {
+}
---
+++ src/main/java/com/musala/javacourse181112/tasks/methodsExercise/PersonService.java
@@ -0,0 +1,15 @@
+package com.musala.javacourse181112.tasks.methodsExercise;
+
+public class PersonService {
+    //    **** static method
+//                ***** name printPersonNamesToStdout
+//                ***** use appropriate access modifier (scope of visibility)
+//                ***** print arguments to stdout, using space discriminator then new line
+//    **** non-static methods
+//                ***** addPerson, accepting and storing Person object to queue
+//                ***** handlePerson, polling one from queue then printing it to stdout (hint: method composition), then returning the person polled
+//                ***** think of multi-threading, always in mind
+    public static void printPersonNamesToStdout() {
+
+    }
+}
