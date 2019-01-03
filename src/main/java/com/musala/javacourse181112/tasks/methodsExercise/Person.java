695716f047b64eaf8ba711e57b6884c19626b7db
Mirella Krysteva <mirella.krysteva@gmail.com>
2019-01-03 19:29:41 +0200

    StaticNonStaticMethodsExercise  - empty main

--- src/main/java/com/musala/javacourse181112/tasks/methodsExercise/Person.java
+++ src/main/java/com/musala/javacourse181112/tasks/methodsExercise/Person.java
@@ -1,4 +1,13 @@
 package com.musala.javacourse181112.tasks.methodsExercise;
 
 public class Person {
+    private String personName;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
 }
--- src/main/java/com/musala/javacourse181112/tasks/methodsExercise/StaticNonstaticMethodsExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/methodsExercise/StaticNonstaticMethodsExercise.java
@@ -1,23 +1,15 @@
 package com.musala.javacourse181112.tasks.methodsExercise;
 
 public class StaticNonstaticMethodsExercise {
-//        *** create a task unique package (subpackage of com.musala.javacourse181112.tasks, think about naming)
-//        *** add a Person class (entity)
-//        *** add a class PersonService, implementing:
-//            **** static method
-//                ***** name printPersonNamesToStdout
-//                ***** use appropriate access modifier (scope of visibility)
-//                ***** print arguments to stdout, using space discriminator then new line
-//            **** non-static methods
-//                ***** addPerson, accepting and storing Person object to queue
-//                ***** handlePerson, polling one from queue then printing it to stdout (hint: method composition), then returning the person polled
-//                ***** think of multi-threading, always in mind
-//        *** add StaticNonstaticMethodsExercise class, and implement main method:
 //            **** instantiate 10 Person instances (loop) and add to collection
 //            **** instantiate 2 PersonService objects
 //            **** call service static and non-static methods, using populated persons
 //            **** use service static method and print directly a new person instance with assigned properties
-//
-//        *** Bonus: implement non-static finalize method, reseting the PersonService instances state at the end of usage
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Person();
+        }
+    }
 
 }
