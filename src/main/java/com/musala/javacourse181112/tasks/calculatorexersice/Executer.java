2772293601c021676f30f0a9d4ca454180c5c43d
BorisBNikolov <nikolov.b.boirs@gmail.com>
2019-01-03 18:48:51 +0200

    Calculator

--- src/main/java/com/musala/javacourse181112/tasks/calculatorexersice/Executer.java
+++ src/main/java/com/musala/javacourse181112/tasks/calculatorexersice/Executer.java
@@ -8,16 +8,16 @@ public class Executer {
         List<Person> people = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            int numberOfTheChar=65+i;
-            people.add(new Person((char) numberOfTheChar+ ". " +(char) numberOfTheChar+".",
-                    i + (int)(Math.random()*50)));
+            int numberOfTheChar = 65 + i;
+            people.add(new Person((char) numberOfTheChar + ". " + (char) numberOfTheChar + ".",
+                    i + (int) (Math.random() * 50)));
         }
 
         Calculator calculator = new Calculator();
         for (Person person : people) {
             calculator.setPerson(person);
             calculator.setYearsToAdd(10);
-            System.out.println("Person name:"+person.getName() + " His/Her Future Age:" + calculator.getFutureAge());
+            System.out.println("Person name:" + person.getName() + " His/Her Future Age:" + calculator.getFutureAge());
         }
     }
 }
