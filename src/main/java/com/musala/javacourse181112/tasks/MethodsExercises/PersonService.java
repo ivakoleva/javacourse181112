d9df08adda710acd362092b525cf07dee2a88d78
dgluharov <dgluharov@gmail.com>
2019-01-03 14:09:36 +0200

    Methods homework

--- src/main/java/com/musala/javacourse181112/tasks/MethodsExercises/PersonService.java
+++ src/main/java/com/musala/javacourse181112/tasks/MethodsExercises/PersonService.java
@@ -1,5 +1,19 @@
 package com.musala.javacourse181112.tasks.MethodsExercises;
 
+/**
+ * *** static method
+ * **** name printPersonNamesToStdout
+ * **** use appropriate access modifier (scope of visibility)
+ * **** print arguments to stdout, using space discriminator then new line
+ * *** non-static methods
+ * **** addPerson, accepting and storing Person object to queue
+ * **** handlePerson, polling one from queue then printing it to stdout (hint: method composition),
+ * then returning the person polled
+ * **** think of multi-threading, always in mind
+ */
 public class PersonService {
+    static void printPersonNameToStdout(final Person person) {
+        System.out.println(person.getPersonName());
+    }
 
 }
