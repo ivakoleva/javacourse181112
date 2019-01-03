550bf168cb558581c79b8cedb3ef3248f2f2737a
BorisBNikolov <nikolov.b.boirs@gmail.com>
2019-01-03 19:30:23 +0200

    Calculator + RandomName

--- src/main/java/com/musala/javacourse181112/tasks/calculatorexersice/Executer.java
+++ src/main/java/com/musala/javacourse181112/tasks/calculatorexersice/Executer.java
@@ -9,7 +9,7 @@ public class Executer {
 
         for (int i = 0; i < 10; i++) {
             int numberOfTheChar = 65 + i;
-            people.add(new Person((char) numberOfTheChar + ". " + (char) numberOfTheChar + ".",
+            people.add(new Person(randomFullName(2),
                     i + (int) (Math.random() * 50)));
         }
 
@@ -20,5 +20,25 @@ public class Executer {
             calculator.setYearsToAdd(10);
             System.out.println("Person name:" + person.getName() + " His/Her Future Age:" + calculator.getFutureAge());
         }
+
+
+    }
+
+    public static String randomFullName(int numberOfNames){
+        String fullName = new String();
+        for(int i = 0; i<numberOfNames;i++){
+            fullName+=randomName()+" ";
+        }
+        return  fullName;
+    }
+
+    public static String randomName() {
+        int nameLength = (int) (Math.random() * 7 + 3);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < nameLength; i++) {
+            int charLength = (int) (Math.random() * 26 + 97);
+            builder.append((char) charLength);
+        }
+        return builder.toString().substring(0, 1).toUpperCase()+builder.toString().substring(1);
     }
 }
