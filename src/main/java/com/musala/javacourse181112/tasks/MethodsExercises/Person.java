030cb2813b5a4633b9689da3995ea5ae1ed8c46a
dgluharov <dgluharov@gmail.com>
2019-01-03 13:55:36 +0200

    Methods homework

---
+++ src/main/java/com/musala/javacourse181112/tasks/MethodsExercises/Person.java
@@ -0,0 +1,13 @@
+package com.musala.javacourse181112.tasks.MethodsExercises;
+
+public class Person {
+    private String personName;
+
+    String getPersonName() {
+        return personName;
+    }
+
+    void setPersonName(String personName) {
+        this.personName = personName;
+    }
+}
---
+++ src/main/java/com/musala/javacourse181112/tasks/MethodsExercises/PersonService.java
@@ -0,0 +1,5 @@
+package com.musala.javacourse181112.tasks.MethodsExercises;
+
+public class PersonService {
+
+}
