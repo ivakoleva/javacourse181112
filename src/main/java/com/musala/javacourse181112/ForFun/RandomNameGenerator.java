98bbf894f30251a9681be94b562211d2e41261eb
VermilionPride <dimitar.ang.mgu@gmail.com>
2019-01-06 12:20:50 +0200

    Claculator

---
+++ src/main/java/com/musala/javacourse181112/ForFun/RandomNameGenerator.java
@@ -0,0 +1,15 @@
+package com.musala.javacourse181112.ForFun;
+
+public class RandomNameGenerator {
+    public static void main(String[] args) {
+
+    }
+    public char nameGenerator(){
+        int i, b;
+        char[] name= new char[8];
+        for(i=0;i<name.length;i++){
+            for(b=97;b<122;)
+        }
+
+    }
+}
