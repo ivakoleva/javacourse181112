3004a50e08da8e96332531958ab628252893689c
VermilionPride <dimitar.ang.mgu@gmail.com>
2019-01-06 16:43:48 +0200

    Voteapp 4.0

---
+++ src/main/java/com/musala/javacourse181112/tasks/vote40/Vote.java
@@ -0,0 +1,34 @@
+package com.musala.javacourse181112.tasks.vote40;
+
+import java.util.Random;
+
+public class Vote {
+    protected boolean answer;
+    int questionNumber;
+
+    public Vote(int questionNumber) {
+        this.answer = randomAnswer();
+        this.questionNumber = questionNumber;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
+
+    public int getQuestionNumber() {
+        return questionNumber;
+    }
+
+    public void setQuestionNumber(int questionNumber) {
+        this.questionNumber = questionNumber;
+    }
+
+    protected boolean randomAnswer() {
+        Random randomBooleanAnswer = new Random();
+        return randomBooleanAnswer.nextBoolean();
+    }
+}
---
+++ src/main/java/com/musala/javacourse181112/tasks/vote40/mainFunction.java
@@ -0,0 +1,13 @@
+package com.musala.javacourse181112.tasks.vote40;
+
+public class mainFunction {
+    public static void main(final String[] args) {
+        Voter voter1 = new Voter(1);
+        Voter voter2 = new Voter(1);
+
+        System.out.println("Name:" + voter1.getRandomName() + " EGN:" +
+                voter1.getRandomEGN() + " answered: " + voter1.randomAnswer());
+        System.out.println("Name:" + voter2.getRandomName() + " EGN:" +
+                voter2.getRandomEGN() + " answered: " + voter2.randomAnswer());
+    }
+}
