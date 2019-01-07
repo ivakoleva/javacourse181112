f72302342de3ecb83f51a37bf6712354640de517
Bzahov98 <bzahov1998@gmail.com>
2019-01-07 19:32:30 +0200

    [ADD] v0.5 Vote

--- src/main/java/com/musala/javacourse181112/tasks/Votes05DataPopulationExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/Votes05DataPopulationExercise.java
@@ -1,5 +1,6 @@
 package com.musala.javacourse181112.tasks;
 
+import com.musala.javacourse181112.oop.votesexercise.model.Answer;
 import com.musala.javacourse181112.oop.votesexercise.model.Person;
 import com.musala.javacourse181112.oop.votesexercise.model.Poll;
 import com.musala.javacourse181112.oop.votesexercise.model.Question;
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,6 +24,9 @@ public class Votes05DataPopulationExercise {
 
         final Poll coffeePoll = new Poll();
         coffeePoll.setName("Coffee poll");
+
+        String blblblb = "Blalalal";
+
         coffeePoll.setStartDateTime(LocalDateTime.of(2019, Month.JANUARY, 1, 0, 0));
         coffeePoll.setEndDateTime(LocalDateTime.of(2020, Month.JANUARY, 1, 0, 0));
         coffeePoll.setCreatedBy(ivanIvanov);
@@ -29,12 +34,27 @@ public class Votes05DataPopulationExercise {
         final List<Question> questionList = new ArrayList<>();
         coffeePoll.setQuestionList(questionList);
 
-        final Question question1 = new Question();
-        question1.setName("Do you like coffee?");
-        //question1.setAnswerList();
+        populateQuestions(questionList,"Do you like coffee?","Yes","No");
+        populateQuestions(questionList,"How do you enjoy your coffee?","Milk","Sugar","Milk & sugar","Black","None of the above");
+        populateQuestions(questionList,"How many coffees do you have a day?","Less or equal of 2","More than 2 but less than 5","More or equal of 5","None of the above");
+        coffeePoll.setQuestionList(questionList);
+        questionList.forEach(element-> element.getAnswerList().forEach( element2-> System.out.println(element2.getName())));
 
-        questionList.add(question1);
+        // TODO: populateQuestions 3 questions, consult tasks.md
+    }
 
-        // TODO: populate 3 questions, consult tasks.md
+    private static void populateQuestions(List<Question> questionList, String questName, String ... answers) {
+        final Question question = new Question();
+        question.setName(questName);
+        question.setAnswerList(new ArrayList<>());
+        Arrays.stream(answers).forEach(answer->getAndPopAnswer(question,answer));
+        questionList.add(question);
+    }
+
+    private static Answer getAndPopAnswer(Question question, String yes) {
+        Answer answer = new Answer();
+        answer.setName(yes);
+        question.getAnswerList().add(answer);
+        return answer;
     }
 }
