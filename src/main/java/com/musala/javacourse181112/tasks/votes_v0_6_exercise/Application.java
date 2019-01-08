9be99dcf50da79ff0984314de855dd9d6da533e0
YordanRadkov <iordanradkov@gmail.com>
2019-01-08 14:10:49 +0200

    Homework:Votes v0.6   Complete data population and implement PersonService, then  refactor current Person instantiation

--- src/main/java/com/musala/javacourse181112/tasks/votes_v0_6_exercise/Application.java
+++ src/main/java/com/musala/javacourse181112/tasks/votes_v0_6_exercise/Application.java
@@ -2,6 +2,7 @@ package com.musala.javacourse181112.tasks.votes_v0_6_exercise;
 
 import com.musala.javacourse181112.tasks.votes_v0_6_exercise.model.Person;
 import com.musala.javacourse181112.tasks.votes_v0_6_exercise.model.Poll;
+import com.musala.javacourse181112.tasks.votes_v0_6_exercise.service.PersonService;
 import com.musala.javacourse181112.tasks.votes_v0_6_exercise.service.PollService;
 
 import java.time.LocalDate;
@@ -12,15 +13,16 @@ import java.time.Month;
  */
 public class Application {
 
-
     public static void main(final String[] args) {
         final PollService pollService = new PollService();
 
-        // TODO: implement PersonService, and generatePerson() method
-        final Person person1 = new Person();
-        person1.setName("Ivan");
-        person1.setFamily("Ivanov");
-        person1.setDateOfBirth(LocalDate.of(1990, Month.JANUARY, 1));
+        final PersonService personService = new PersonService();
+
+        final Person person = personService.generatePerson(
+                "Ivan",
+                "Ivanov",
+                LocalDate.of(1990, Month.JANUARY, 1)
+        );
 
         final Poll coffeePoll = pollService.generatePoll(
                 "Coffee poll",
@@ -34,10 +36,16 @@ public class Application {
                         () -> pollService.generateAnswer("Sugar"),
                         () -> pollService.generateAnswer("Milk & sugar"),
                         () -> pollService.generateAnswer("Black"),
-                        () -> pollService.generateAnswer("None of the above"))
-                // TODO: implement the rest of the questions & answers
+                        () -> pollService.generateAnswer("None of the above")),
+                () -> pollService.generateQuestion(
+                        "How many coffees do you have a day?",
+                        () -> pollService.generateAnswer("Less or equal of 2"),
+                        () -> pollService.generateAnswer("More than 2 but less than 5"),
+                        () -> pollService.generateAnswer("More or equal of 5"),
+                        () -> pollService.generateAnswer("None")
+                )
         );
-        coffeePoll.setCreatedBy(person1);
+        coffeePoll.setCreatedBy(person);
 
         System.out.println();
     }
---
+++ src/main/java/com/musala/javacourse181112/tasks/votes_v0_6_exercise/service/PersonService.java
@@ -0,0 +1,16 @@
+package com.musala.javacourse181112.tasks.votes_v0_6_exercise.service;
+
+import com.musala.javacourse181112.tasks.votes_v0_6_exercise.model.Person;
+
+import java.time.LocalDate;
+
+public class PersonService {
+
+    public Person generatePerson(final String name, final String family,final LocalDate dateOfBirth){
+        final Person person = new Person();
+        person.setName(name);
+        person.setFamily(family);
+        person.setDateOfBirth(dateOfBirth);
+        return person;
+    }
+}
