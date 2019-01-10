07dd279d4bfab0aa92ad113c6ed7f6dcae2ca1e1
Blueshadows <blueshadows98@gmail.com>
2019-01-10 16:31:22 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/Votes05DataPopulationExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/Votes05DataPopulationExercise.java
@@ -21,10 +21,10 @@ public class Votes05DataPopulationExercise {
         ivanIvanov.setFamily("Ivanov");
         ivanIvanov.setDateOfBirth(LocalDate.of(1990, Month.JANUARY, 1));
 
-        final Person todorJivkov= new Person();
+        final Person todorJivkov = new Person();
         todorJivkov.setName("Todor");
         todorJivkov.setFamily("Jivkov");
-        todorJivkov.setDateOfBirth(LocalDate.of(1911,Month.SEPTEMBER,7));
+        todorJivkov.setDateOfBirth(LocalDate.of(1911, Month.SEPTEMBER, 7));
 
         final Poll coffeePoll = new Poll();
         coffeePoll.setName("Coffee poll");
@@ -34,73 +34,72 @@ public class Votes05DataPopulationExercise {
 
         final List<Question> questionList = new ArrayList<>();
         coffeePoll.setQuestionList(questionList);
-    //QUESTION 1
+        //QUESTION 1
         final Question question1 = new Question();
         question1.setName("Do you like coffee?");
 
-        final List<Answer> answerList1=new ArrayList<>();
+        final List<Answer> answerList1 = new ArrayList<>();
         question1.setAnswerList(answerList1);
 
-        final Answer answerYes=new Answer();
+        final Answer answerYes = new Answer();
         answerYes.setName("Yes");
         answerList1.add(answerYes);
 
-        final Answer answerNo=new Answer();
+        final Answer answerNo = new Answer();
         answerYes.setName("No");
         answerList1.add(answerNo);
 
         questionList.add(question1);
-    //QUESTION 2
-        final Question question2 =new Question();
+        //QUESTION 2
+        final Question question2 = new Question();
         question2.setName("How do you enjoy your coffee?");
 
-        final List<Answer> answerList2=new ArrayList<>();
+        final List<Answer> answerList2 = new ArrayList<>();
         question2.setAnswerList(answerList2);
 
-        final Answer answerMilk=new Answer();
+        final Answer answerMilk = new Answer();
         answerMilk.setName("Milk");
         answerList2.add(answerMilk);
 
-        final Answer answerSugar=new Answer();
+        final Answer answerSugar = new Answer();
         answerSugar.setName("Sugar");
         answerList2.add(answerSugar);
 
-        final Answer answerMilkSugar=new Answer();
+        final Answer answerMilkSugar = new Answer();
         answerMilkSugar.setName("MilkSugar");
         answerList2.add(answerMilkSugar);
 
-        final Answer answerBlack=new Answer();
+        final Answer answerBlack = new Answer();
         answerBlack.setName("Black");
         answerList2.add(answerBlack);
 
-        final Answer answerNone=new Answer();
+        final Answer answerNone = new Answer();
         answerNone.setName("None");
         answerList2.add(answerNone);
 
         questionList.add(question2);
         //QUESTION 3
-        final Question question3 =new Question();
+        final Question question3 = new Question();
         question2.setName("How many coffees do you have a day?");
 
-        final List<Answer> answerList3=new ArrayList<>();
+        final List<Answer> answerList3 = new ArrayList<>();
         question3.setAnswerList(answerList3);
 
-        final Answer answer1=new Answer();
+        final Answer answer1 = new Answer();
         answer1.setName("Less or equal of 2");
         answerList3.add(answerMilk);
 
-        final Answer answer2=new Answer();
+        final Answer answer2 = new Answer();
         answer2.setName("More than 2 but less than 5");
         answerList3.add(answer2);
 
-        final Answer answer3=new Answer();
+        final Answer answer3 = new Answer();
         answer3.setName("More or equal of 5");
         answerList3.add(answer3);
 
-        final Answer answer4=new Answer();
+        final Answer answer4 = new Answer();
         answer4.setName("None");
         answerList3.add(answer4);
-
 
 
         questionList.add(question3);
