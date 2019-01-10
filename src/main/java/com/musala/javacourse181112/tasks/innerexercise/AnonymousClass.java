022b0c75211f69b93f8aca55801b5a85d1a14adc
Kristina <miss.krisi@abv.bg>
2019-01-10 18:50:40 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/innerexercise/AnonymousClass.java
+++ src/main/java/com/musala/javacourse181112/tasks/innerexercise/AnonymousClass.java
@@ -2,12 +2,7 @@ package com.musala.javacourse181112.tasks.innerexercise;
 
 public class AnonymousClass  {
     public static void main(String[] args) {
-        final Runnable anonymousRunnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("This is an anonymous cat.");
-            }
-        };
+        final Runnable anonymousRunnable = () -> System.out.println("This is an anonymous cat.");
         anonymousRunnable.run();
     }
 }
