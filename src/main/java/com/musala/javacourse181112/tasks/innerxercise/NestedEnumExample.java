53b390597458a10d1523548b9c5adfb915c79b82
Bzahov98 <bzahov1998@gmail.com>
2019-01-10 17:49:04 +0200

    [ADD] add Nested objects examples

---
+++ src/main/java/com/musala/javacourse181112/tasks/innerxercise/NestedEnumExample.java
@@ -0,0 +1,9 @@
+package com.musala.javacourse181112.tasks.innerxercise;
+
+public enum  NestedEnumExample {
+    A,B,C,D;
+
+    enum NestedEnum{
+      E,F,G  ;
+    }
+}
---
+++ src/main/java/com/musala/javacourse181112/tasks/innerxercise/NestedInterfaceExample.java
@@ -0,0 +1,10 @@
+package com.musala.javacourse181112.tasks.innerxercise;
+
+public interface NestedInterfaceExample {
+
+    String doSomething();
+
+    interface NestedInterface{
+        String doNothing();
+    }
+}
