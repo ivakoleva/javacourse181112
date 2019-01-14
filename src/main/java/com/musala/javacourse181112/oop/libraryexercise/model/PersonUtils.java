1aca56f660816b018435711826a867841cafcc0b
Bzahov98 <bzahov1998@gmail.com>
2019-01-14 20:28:25 +0200

    [Update] PersonUtils simplified, and add generics

--- src/main/java/com/musala/javacourse181112/oop/libraryexercise/model/PersonUtils.java
+++ src/main/java/com/musala/javacourse181112/oop/libraryexercise/model/PersonUtils.java
@@ -1,27 +1,23 @@
 package com.musala.javacourse181112.oop.libraryexercise.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PersonUtils {
 
-    public static List<Person> compareByDate(List<Person>people){
-
-        List<Person> result = new ArrayList<>();
-        Collections.copy(result,people);
-
-        result.sort(Comparator.comparing(person -> findValidSubscription(person).getDateTimeOfPayment()));
-        return result;
+    public static <T extends Person> List<T> compareByDate(List<T>people){
+        return people.stream()
+                .filter(Objects::nonNull)
+                .sorted(Comparator.comparing(person -> findValidSubscription((T)person).getDateTimeOfPayment()).reversed())
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public static SubscriptionRenewal findValidSubscription(Person person){
+    public static <T extends Person>SubscriptionRenewal findValidSubscription(T person){
             if (person == null || person.getSubscriptionRenewalSet() == null || person.getSubscriptionRenewalSet().isEmpty()){
                 return null;
             }
-            SubscriptionRenewal subscriptionRenewal = person.getSubscriptionRenewalSet().stream().max(Comparator.comparing(SubscriptionRenewal::getDateTimeOfPayment)).get();
-                assert  subscriptionRenewal.getDateTimeOfPayment() != null;
-            return subscriptionRenewal;
+            Set<SubscriptionRenewal>  subscriptionRenewal = person.getSubscriptionRenewalSet();
+
+            return subscriptionRenewal.stream().max(Comparator.comparing(SubscriptionRenewal::getDateTimeOfPayment)).get();
     }
 }
