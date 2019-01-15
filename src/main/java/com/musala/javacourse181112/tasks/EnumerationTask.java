81637623abadc0f098eb6df456be5aec3c6310d8
Blueshadows <blueshadows98@gmail.com>
2019-01-15 18:23:53 +0200

    EnumerationTask

---
+++ src/main/java/com/musala/javacourse181112/tasks/EnumerationTask.java
@@ -0,0 +1,29 @@
+package com.musala.javacourse181112.tasks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+public class EnumerationTask {
+    public static void main(final String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+
+        Enumeration<String> enumeration;
+
+        stringList.add("String 1");
+        stringList.add("String 2");
+
+        enumeration = Collections.enumeration(stringList);
+
+
+        while (enumeration.hasMoreElements()) {
+            System.out.println("Enumeration: "+enumeration.nextElement());
+        }
+
+        ArrayList<String>stringList1 = Collections.list(enumeration);
+        stringList1.forEach(System.out::println);
+
+    }
+}
