8ea7ed1ecfca20cbed43e2e8013f70e1de69f18e
dgluharov <dgluharov@gmail.com>
2019-01-15 18:14:15 +0200

    Enumeration exercise

--- src/main/java/com/musala/javacourse181112/tasks/EnumerationCollectionExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/EnumerationCollectionExercise.java
@@ -6,20 +6,21 @@ import java.util.Enumeration;
 import java.util.List;
 
 public class EnumerationCollectionExercise {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+    public static void main(final String[] args) {
+        final List<String> list = new ArrayList<>();
         list.add("one");
         list.add("two");
         list.add("three");
         list.add("four");
         list.add("five");
 
-        Enumeration enumeration = Collections.enumeration(list);
+        final Enumeration enumeration = Collections.enumeration(list);
 
-        while (enumeration.hasMoreElements())
+        while (enumeration.hasMoreElements()) {
             System.out.println(enumeration.nextElement());
+        }
 
-        ArrayList<String> arrayNewList = Collections.list(enumeration);
+        final ArrayList<String> arrayNewList = Collections.list(enumeration);
         System.out.println("List elements: " + list);
     }
 }
