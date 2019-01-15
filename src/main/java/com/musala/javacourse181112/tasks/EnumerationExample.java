b8ac7da1fb16fd11db1524300a3ec2c0247c3fcf
anton kolev <antongtkolev@gmail.com>
2019-01-15 18:05:00 +0200

    Enumartion Example exercise

---
+++ src/main/java/com/musala/javacourse181112/tasks/EnumerationExample.java
@@ -0,0 +1,28 @@
+package com.musala.javacourse181112.tasks;
+
+import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class EnumerationExample {
+
+    public static void main(String[] args) {
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("D");
+        arrayList.add("E");
+        arrayList.add("F");
+        arrayList.add("G");
+
+        Enumeration e = Collections.enumeration(arrayList);
+
+        System.out.println("Arraylist to Enumeration");
+        while(e.hasMoreElements())
+            System.out.println(e.nextElement());
+
+        ArrayList<String> arrayList1 = Collections.list(e);
+        System.out.println("List elements: "+ arrayList);
+    }
+}
