44cf5e49d67c62ea4d59015503faaef6903b344f
YordanRadkov <iordanradkov@gmail.com>
2019-01-15 18:16:46 +0200

    Enumeration-collection bridge exercise

--- src/main/java/com/musala/javacourse181112/tasks/EnumerationCollectionBridgeExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/EnumerationCollectionBridgeExercise.java
@@ -1,4 +1,23 @@
 package com.musala.javacourse181112.tasks;
 
-public class EnumarionExercise {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+public class EnumerationCollectionBridgeExercise {
+
+    public static void main(final String[] args) {
+        final List <Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+
+        final Enumeration<Integer> integerEnumeration = Collections.enumeration(integerList);
+
+        while (integerEnumeration.hasMoreElements()){
+            System.out.println(integerEnumeration.nextElement());
+        }
+
+        Collections.list(integerEnumeration);
+    }
 }
