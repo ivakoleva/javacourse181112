2fa46c2faa4fd3691214872332cc5d2999a033ac
dgluharov <dgluharov@gmail.com>
2019-01-15 17:37:45 +0200

    Library Exercise

---
+++ src/main/java/com/musala/javacourse181112/tasks/libraryexercise/model/Issueable.java
@@ -0,0 +1,10 @@
+package com.musala.javacourse181112.tasks.libraryexercise.model;
+
+public interface Issueable {
+    int getIssue();
+
+    void setIssue(int issue);
+
+}
+
+
--- src/main/java/com/musala/javacourse181112/tasks/libraryexercise/model/Newspaper.java
+++ src/main/java/com/musala/javacourse181112/tasks/libraryexercise/model/Newspaper.java
@@ -1,4 +1,15 @@
 package com.musala.javacourse181112.tasks.libraryexercise.model;
 
-public class Newspaper extends AbstractItem {
+public class Newspaper extends AbstractItem implements Issueable {
+    public int issue;
+
+    @Override
+    public int getIssue() {
+        return issue;
+    }
+
+    @Override
+    public void setIssue(int issue) {
+        this.issue = issue;
+    }
 }
