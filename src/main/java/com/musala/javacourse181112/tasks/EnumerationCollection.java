c116eb686d567a920192eeed311116a3348c67a2
muzun97 <muzun@abv.bg>
2019-01-15 17:55:02 +0200

    Enumeration-collection exercise

---
+++ src/main/java/com/musala/javacourse181112/tasks/EnumerationCollection.java
@@ -0,0 +1,27 @@
+package com.musala.javacourse181112.tasks;
+
+import javafx.print.Collation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+public class EnumerationCollection {
+    public static void main(final String[] args) {
+
+        List arrayList = new ArrayList();
+
+        for (int i = 1; i <= 3; i++) {
+            arrayList.add(i);
+        }
+
+        Enumeration e = Collections.enumeration(arrayList);
+        while(e.hasMoreElements()) {
+            System.out.println("Value is: "+e.nextElement());
+        }
+
+        List list = Collections.list(e);
+
+    }
+}
