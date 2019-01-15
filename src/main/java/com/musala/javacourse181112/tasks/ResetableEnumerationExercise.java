d118ecbe5d725a50d972e6ea402dad0b0c009ca2
IcouldEsmileof <aykut@mail.bg>
2019-01-15 19:38:53 +0200

    Resetable Enumeration Exercise

--- src/main/java/com/musala/javacourse181112/tasks/ResetableEnumerationExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/ResetableEnumerationExercise.java
@@ -3,37 +3,58 @@ package com.musala.javacourse181112.tasks;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 /**
  * Created by Iva Koleva on 15.01.2019
  */
 public class ResetableEnumerationExercise {
     public static void main(final String[] args) {
-        // TODO: instantiate custom enumeration using private enumeration method
+        List<Integer> list = IntStream.range(1, 6).boxed().collect(Collectors.toList());
+        ResetableEnumeration<Integer> resetableEnumeration = (ResetableEnumeration<Integer>) enumeration(list);
+        for (; resetableEnumeration.hasMoreElements(); ) {
+            System.out.println(resetableEnumeration.nextElement());
+        }
+        resetableEnumeration.reset();
     }
 
-    // TODO: fix interface surface
+
     private static <T> Enumeration<T> enumeration(final Collection<T> c) {
-        // TODO: instantiate ResetableEnumeration
-        return new Enumeration<T>() {
-            private Iterator<T> i = c.iterator(); // TODO: fix encapsulation - do not duplicate code
-
-            public boolean hasMoreElements() {
-                return i.hasNext();
-            }
-
-            public T nextElement() {
-                return i.next();
-            }
-
-            public void reset() {
-                i = c.iterator();
-            }
-        };
+        return new ResetableEnumeration<T>(c);
     }
 
-    // TODO: implements Enumeration<T>
-    private static class ResetableEnumeration<T> {
 
+}
+
+class ResetableEnumeration<T> implements Enumeration<T> {
+    private Collection<T> collection;
+    private Iterator<T> iterator;
+
+    public ResetableEnumeration(Collection<T> collection) {
+        this.collection = collection;
+        iterator = collection.iterator();
     }
+
+    public void setNewCollection(Collection<T> newCollection) {
+        assert newCollection != null;
+
+        this.iterator = newCollection.iterator();
+        this.collection = newCollection;
+    }
+
+    public boolean hasMoreElements() {
+        return iterator.hasNext();
+    }
+
+    public T nextElement() {
+        return iterator.next();
+    }
+
+    public void reset() {
+        iterator = collection.iterator();
+    }
+
 }
