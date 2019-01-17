010e248d7ef10bb20af265c6aec9d0512535d8f2
BorisBNikolov <nikolov.b.boirs@gmail.com>
2019-01-17 15:13:24 +0200

    Homework ProvicesEnum

--- src/main/java/com/musala/javacourse181112/tasks/ProvincesEnum.java
+++ src/main/java/com/musala/javacourse181112/tasks/ProvincesEnum.java
@@ -1,4 +1,53 @@
 package com.musala.javacourse181112.tasks;
 
 public enum ProvincesEnum {
+    SOFIACITY("SofiaGrad", 1291591, 1, PlanningRegion.SOUTH_WESTERN),
+    PLOVDIV("Plovdiv", 683027, 18, PlanningRegion.SOUTH_CENTRAl),
+    BLAGOEVGRAD("Blagoevgrad",323552, 14, PlanningRegion.SOUTH_WESTERN);
+
+
+
+    private String label;
+    private int populataion;
+    private int municipality;
+    private PlanningRegion planningRegion;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public int getPopulataion() {
+        return populataion;
+    }
+
+    public void setPopulataion(int populataion) {
+        this.populataion = populataion;
+    }
+
+    public int getMunicipality() {
+        return municipality;
+    }
+
+    public void setMunicipality(int municipality) {
+        this.municipality = municipality;
+    }
+
+
+    ProvincesEnum(final String label, final int populataion,final int municipality, final PlanningRegion planningRegion) {
+        this.label = label;
+        this.populataion = populataion;
+        this.municipality = municipality;
+        this.planningRegion = planningRegion;
+    }
+
+
+    public enum PlanningRegion {
+        SOUTH_WESTERN,
+        SOUTH_CENTRAl;
+    }
 }
+
