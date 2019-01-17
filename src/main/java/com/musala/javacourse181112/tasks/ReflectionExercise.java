51a793842995e265c4cca6098c2bc9459634cacb
IcouldEsmileof <aykut@mail.bg>
2019-01-17 19:11:22 +0200

    Reflection

--- src/main/java/com/musala/javacourse181112/tasks/ReflectionExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/ReflectionExercise.java
@@ -1,6 +1,8 @@
 package com.musala.javacourse181112.tasks;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Objects;
 
 /*
  ** Reflection generic instantiation
@@ -17,15 +19,15 @@ public class ReflectionExercise {
         String string;
 
         public Sample() {
-            this(0, "");
+            this(-1, "bez_string");
         }
 
         public Sample(Integer integer) {
-            this(integer, "");
+            this(integer, "bez_string");
         }
 
         public Sample(String string) {
-            this(0, string);
+            this(-1, string);
         }
 
         public Sample(Integer integer, String string) {
@@ -39,39 +41,56 @@ public class ReflectionExercise {
     }
 
     public static void main(String[] args) {
-        try {
-            Sample.class.getConstructor(Integer.class, String.class).newInstance(5, "afa");
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
+        /*try {
+            System.out.println(Sample.class.getConstructor(Integer.class, String.class).newInstance(5, "afa"));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             e.printStackTrace();
         }
         try {
-            Sample.class.getConstructor(Integer.class).newInstance(5);
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
+            System.out.println(Sample.class.getConstructor(Integer.class).newInstance(5));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             e.printStackTrace();
         }
         try {
-            Sample.class.getConstructor(String.class).newInstance("afa");
-        } catch (InstantiationException e) {
+            System.out.println(Sample.class.getConstructor(String.class).newInstance("afa"));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        //Arrays.stream(Sample.class.getDeclaredConstructors()).forEach(i->getNeededParameters(i.getParameterTypes()));
+        }*/
+
+        Arrays.stream(Sample.class.getDeclaredConstructors())
+                .map(i -> {
+                    int parameterLength = i.getParameterTypes().length;
+                    Sample sample = null;
+                    if (parameterLength == 2) {
+                        try {
+                            sample = (Sample) i.newInstance(5, "string_i_int");
+                        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    } else if (parameterLength == 1) {
+                        if (i.getParameterTypes()[0].equals(Integer.class)) {
+                            try {
+                                sample = (Sample) i.newInstance(4);
+                            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                                e.printStackTrace();
+                            }
+                        } else if (i.getParameterTypes()[0].equals(String.class)) {
+                            try {
+                                sample = (Sample) i.newInstance("samo_string");
+                            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    } else {
+                        try {
+                            sample = (Sample) i.newInstance();
+                        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    return sample;
+                })
+                .filter(Objects::nonNull)
+                .forEach(System.out::println);
     }
 }
