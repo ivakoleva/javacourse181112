4cc6877f79b880817b42339b0be2382c0a4abd9c
Bzahov98 <bzahov1998@gmail.com>
2019-01-17 19:28:48 +0200

    [Add] GenericsInstation

--- src/main/java/com/musala/javacourse181112/tasks/reflection/GenericsInstance.java
+++ src/main/java/com/musala/javacourse181112/tasks/reflection/GenericsInstance.java
@@ -1,29 +1,45 @@
 package com.musala.javacourse181112.tasks.reflection;
 
+import com.musala.javacourse181112.oop.libraryexercise.model.Person;
+
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GenericsInstance {
 
+    public static void main(String[] args) {
+        Person person = new Person();
+        Integer integer = 1;
+        getAllConstructors(person.getClass(), integer.getClass());
+        getAllInstancesFromConstructors(person.getClass(), integer.getClass());
+    }
+
+
+
+
     // TODO: Not implemented
-    public static <T, U extends Number> Collection<T> getAllInstacesByConstructors(final Class<T> inputClass, final Class<U> compareClass) {
+    public static <T,U extends Number> Collection<T> getAllConstructors(final Class<T> inputClass, final Class<U> compareClass) {
         assert inputClass != null;
-        final Predicate<Class<?>> isAssignablePredicate = param-> param.isAssignableFrom(compareClass);
+        final Predicate<Class<U>> isAssignablePredicate = param-> param.isAssignableFrom(compareClass);
         try {
-            return Collections.EMPTY_LIST;
-            /*toStream(inputClass.getDeclaredConstructors())
+            return toStream(inputClass.getDeclaredConstructors())
                 .filter(constructor -> toStream(constructor.getParameterTypes())
-                        .filter(param-> isAssignablePredicate.test(param)))
-                .collect(Collectors.toList());*/
+                        .filter(param-> isAssignablePredicate.test((Class<U>) param)))
+                .collect(Collectors.toList());
 
         } catch (/*IllegalAccessException | InvocationTargetException |
                 NoSuchMethodException | InstantiationException */ Exception e) {
             System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    // TODO: Not implemented
+    private static<T,U extends Number> void getAllInstancesFromConstructors(Class<T> aClass, Class<U> aClass1) {
+
     }
 
     public static<T> Stream<T> toStream(final T array[]){
