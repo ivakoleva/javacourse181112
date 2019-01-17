e94d23fd25cdb2e6256767eae37f1cf8496bfbc6
Iva Koleva <iva.koleva@clearcode.bg>
2019-01-17 16:34:16 +0200

    homework review

--- src/main/java/com/musala/javacourse181112/tasks/Provinces.java
+++ src/main/java/com/musala/javacourse181112/tasks/Provinces.java
@@ -1,14 +1,12 @@
 package com.musala.javacourse181112.tasks;
 
 public enum Provinces {
-    SOFIA_CITY ("Sofia-city", 11291591, 1, PlanningRegion.SOUTH_WESTERN),
-    PLOVDIV ("Plovdiv", 683027, 18, PlanningRegion.SOUTH_CENTRAL),
-    PAZARDZHIK ("Pazardzhik", 275548, 12, PlanningRegion.SOUTH_CENTRAL)
-    ;
-
+    SOFIA_CITY("Sofia-city", 11291591, 1, PlanningRegion.SOUTH_WESTERN),
+    PLOVDIV("Plovdiv", 683027, 18, PlanningRegion.SOUTH_CENTRAL),
+    PAZARDZHIK("Pazardzhik", 275548, 12, PlanningRegion.SOUTH_CENTRAL);
 
     private String label;
-    private int populataion;
+    private int population;
     private int municipality;
     private PlanningRegion planningRegion;
 
@@ -16,30 +14,21 @@ public enum Provinces {
         return label;
     }
 
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public int getPopulataion() {
-        return populataion;
-    }
-
-    public void setPopulataion(int populataion) {
-        this.populataion = populataion;
+    public int getPopulation() {
+        return population;
     }
 
     public int getMunicipality() {
         return municipality;
     }
 
-    public void setMunicipality(int municipality) {
-        this.municipality = municipality;
+    public PlanningRegion getPlanningRegion() {
+        return planningRegion;
     }
 
-
-    Provinces(final String label, final int populataion,final int municipality, final PlanningRegion planningRegion) {
+    Provinces(final String label, final int population, final int municipality, final PlanningRegion planningRegion) {
         this.label = label;
-        this.populataion = populataion;
+        this.population = population;
         this.municipality = municipality;
         this.planningRegion = planningRegion;
     }
@@ -49,5 +38,4 @@ public enum Provinces {
         SOUTH_WESTERN,
         SOUTH_CENTRAL
     }
-
 }
