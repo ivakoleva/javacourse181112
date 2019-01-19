dbf9e6067b2b94174862d1cc3d9a3c1e388a8140
Blueshadows <blueshadows98@gmail.com>
2019-01-19 12:40:53 +0200

    update

--- src/main/java/com/musala/javacourse181112/tasks/ReflectionExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/ReflectionExercise.java
@@ -1,39 +1,56 @@
 package com.musala.javacourse181112.tasks;
 
-import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+class SampleClass {
+    private String field1;
+    private int field2;
+
+    public SampleClass() {
+    }
+
+    public SampleClass(String field1) {
+        this.field1 = field1;
+    }
+
+    public SampleClass(int field2) {
+        this.field2 = field2;
+    }
+
+    public SampleClass(String field1, int field2) {
+        this.field1 = field1;
+        this.field2 = field2;
+    }
+
+    @Override
+    public String toString() {
+        return "SampleClass{" +
+                "field1='" + field1 + '\'' +
+                ", field2=" + field2 +
+                '}';
+    }
+}
 
 public class ReflectionExercise {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-       final Class<SampleClass> clazz=SampleClass.class;
+        final List<SampleClass> sampleObjects = new ArrayList<>();
 
-       final Constructor<SampleClass>[] constructors= (Constructor<SampleClass>[]) clazz.getConstructors();
+        sampleObjects.add(SampleClass.class
+                .getDeclaredConstructor().newInstance());
 
+        sampleObjects.add(SampleClass.class
+                .getDeclaredConstructor(String.class).newInstance("DAL GOSPOD DOBRO"));
 
+        sampleObjects.add(SampleClass.class
+                .getDeclaredConstructor(int.class).newInstance(3));
 
+        sampleObjects.add(SampleClass.class
+                .getDeclaredConstructor(String.class,int.class).newInstance("DA AM NE",2));
+
+        sampleObjects.forEach(System.out::println);
     }
 
-    class SampleClass {
-        private String field1;
-        private int field2;
 
-        public SampleClass() {
-        }
-
-        public SampleClass(String field1) {
-            this.field1 = field1;
-        }
-
-        public SampleClass(int field2) {
-            this.field2 = field2;
-        }
-
-        public SampleClass(String field1, int field2) {
-            this.field1 = field1;
-            this.field2 = field2;
-        }
-
-
-    }
 
 }
