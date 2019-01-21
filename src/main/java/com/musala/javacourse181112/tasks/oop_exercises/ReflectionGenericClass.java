ec51ceed5f57f85a340dae6e798d746365ccbec5
Iva Koleva <iva.koleva@clearcode.bg>
2019-01-21 16:27:50 +0200

    homework review

--- src/main/java/com/musala/javacourse181112/tasks/oop_exercises/ReflectionGenericClass.java
+++ src/main/java/com/musala/javacourse181112/tasks/oop_exercises/ReflectionGenericClass.java
@@ -1,23 +1,32 @@
 package com.musala.javacourse181112.tasks.oop_exercises;
 
-public class ReflectionGenericClass<E> {
+public class ReflectionGenericClass<E> implements Cloneable {
     private E value;
 
     public ReflectionGenericClass() {
-
     }
 
-    public ReflectionGenericClass (E value){
+    public ReflectionGenericClass(E value) {
         this.value = value;
     }
 
-    public E getValue(){
+    public E getValue() {
         return value;
     }
-    public void setValue(E value){
+
+    public void setValue(E value) {
         this.value = value;
     }
-    public static <T> ReflectionGenericClass<T> copy (ReflectionGenericClass<T> reflectionGenericClass){
-        return new ReflectionGenericClass<T>(reflectionGenericClass.getValue());
+
+    @Override
+    public ReflectionGenericClass<E> clone() throws CloneNotSupportedException {
+        final ReflectionGenericClass<E> clone = (ReflectionGenericClass<E>) super.clone();
+        clone.setValue(value);
+        return clone;
+        //return new ReflectionGenericClass(value);
     }
+
+    /*public <T> ReflectionGenericClass<T> clone(ReflectionGenericClass<T> reflectionGenericClass) {
+        return new ReflectionGenericClass<T>(reflectionGenericClass.getValue());
+    }*/
 }
--- src/main/java/com/musala/javacourse181112/tasks/oop_exercises/ReflectionGenericClassTester.java
+++ src/main/java/com/musala/javacourse181112/tasks/oop_exercises/ReflectionGenericClassTester.java
@@ -4,17 +4,22 @@ public class ReflectionGenericClassTester {
 
     //this class can extend ReflectionGenericClass<String> for example
 
-    public static void main(String[] args) {
-        ReflectionGenericClass<Integer> integerReflectionGenericClass = new ReflectionGenericClass<>();
-        ReflectionGenericClass<Double> doubleReflectionGenericClass = new ReflectionGenericClass<>();
-        ReflectionGenericClass<String> stringReflectionGenericClass = new ReflectionGenericClass<>();
+    public static void main(String[] args) throws CloneNotSupportedException {
+        final ReflectionGenericClass<Integer> integerReflectionGenericClass = new ReflectionGenericClass<>();
+        final ReflectionGenericClass<Double> doubleReflectionGenericClass = new ReflectionGenericClass<>();
+        final ReflectionGenericClass<String> stringReflectionGenericClass = new ReflectionGenericClass<>();
 
-        integerReflectionGenericClass.setValue(new Integer(10));
-        doubleReflectionGenericClass.setValue(new Double(25.20));
-        stringReflectionGenericClass.setValue(new String("hello"));
+        integerReflectionGenericClass.setValue(10);
+        doubleReflectionGenericClass.setValue(25.20);
+        stringReflectionGenericClass.setValue("hello");
 
         System.out.println("Integer value: " + integerReflectionGenericClass.getValue());
         System.out.println("Double value: " + doubleReflectionGenericClass.getValue());
         System.out.println("String value: " + stringReflectionGenericClass.getValue());
+
+        final ReflectionGenericClass<Integer> clone1 = integerReflectionGenericClass.clone();
+        final ReflectionGenericClass<Double> clone2 = doubleReflectionGenericClass.clone();
+        final ReflectionGenericClass<String> clone3 = stringReflectionGenericClass.clone();
+        System.out.println();
     }
 }
