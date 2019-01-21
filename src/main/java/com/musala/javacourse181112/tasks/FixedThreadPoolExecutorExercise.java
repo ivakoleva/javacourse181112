061b80c718b8457d9decb58153bb211369b7ce35
BorisBNikolov <nikolov.b.boirs@gmail.com>
2019-01-21 18:52:22 +0200

    Fixed thread pool executor exercise

---
+++ src/main/java/com/musala/javacourse181112/tasks/FixedThreadPoolExecutorExercise.java
@@ -0,0 +1,27 @@
+package com.musala.javacourse181112.tasks;
+
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FixedThreadPoolExecutorExercise {
+    public static void main(String[] args) {
+        final ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        final Callable callable = () -> atomicInteger.getAndIncrement();
+
+        /*final List<Future<Integer>> futureList = (List<Future<Integer>>) IntStream.range(0, 10).boxed()
+                .map(i -> callable)
+                .map(executorService::submit)
+                .collect(Collectors.toList());*/
+        final List<Future<Integer>> futureList = IntStream.range(0, 10).boxed()
+             .map(i -> executorService.submit(callable))
+             .collect(Collectors.toList());
+
+        executorService.shutdown();
+
+    }
+}
