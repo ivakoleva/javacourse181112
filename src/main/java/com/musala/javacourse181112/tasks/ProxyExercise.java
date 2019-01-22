0b13ac5ee09011b94402801c5d0511b3ef1f2feb
Mirella Krysteva <mirella.krysteva@gmail.com>
2019-01-22 13:38:10 +0200

    ProxyExercise broken

---
+++ src/main/java/com/musala/javacourse181112/tasks/ProxyExercise.java
@@ -0,0 +1,67 @@
+package com.musala.javacourse181112.tasks;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class ProxyExercise {
+    /*
+     *** intercept all setters of String values
+     *** then modify the value to upper-case
+     *** set the modified value
+     *** print instance fields to stdout
+     * */
+    public static void main(final String[] args) {
+        final ProxyExercise.StingInterface stringInstance = (ProxyExercise.StingInterface) Proxy.newProxyInstance(
+                ClassLoader.getSystemClassLoader(),
+                new Class[]{
+                        ProxyExercise.StingInterface.class
+                },
+                new ProxyExercise.SampleInvocationHandler(new ProxyExercise.StringClass())
+        );
+    }
+
+    private static class SampleInvocationHandler implements InvocationHandler {
+        private Object instance;
+
+        public SampleInvocationHandler(final Object instance) {
+            this.instance = instance;
+        }
+
+        @Override
+        public Object invoke(final Object proxy,
+                             final Method method,
+                             final Object[] args) throws Throwable {//getParameterTypes() да се провери дали е 1 стринг
+            if ((method.getName().startsWith("set")) &&
+                    (method.getParameterTypes().getClass().equals(String)) &&
+                    (args != null)) {
+                method.getName().toUpperCase();
+            }
+//            System.out.println("Method " + method.getName() +
+//                    " with args [" + Arrays.stream(args).map(Object::toString).collect(Collectors.joining(", ")) +
+//                    "] intercepted.");
+            return method.invoke(instance, args);
+        }
+    }
+
+
+    private interface StingInterface {
+        String getStringValue();
+
+        void setStringValue(String stringValue);
+    }
+
+    private static class StringClass implements StingInterface {
+        private String stringValue;
+
+        @Override
+        public String getStringValue() {
+            return stringValue;
+        }
+
+        @Override
+        public void setStringValue(String stringValue) {
+            this.stringValue = stringValue;
+        }
+    }
+}
