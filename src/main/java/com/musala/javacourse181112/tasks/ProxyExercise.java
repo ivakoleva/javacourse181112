acb8b5ccb6038337dc172c14daec723a7c942a4e
Iva Koleva <iva.koleva@clearcode.bg>
2019-01-22 16:31:16 +0200

    homework review

--- src/main/java/com/musala/javacourse181112/tasks/ProxyExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/ProxyExercise.java
@@ -4,32 +4,33 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-
 public class ProxyExercise {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         final StringPropertyInterface stringInstance = (StringPropertyInterface) Proxy.newProxyInstance(
                 ClassLoader.getSystemClassLoader(),
                 new Class[]{
                         StringPropertyInterface.class
                 },
-                new OneInvocationHandler(new OneClass())
+                new OneInvocationHandler<>(new OneClass())
         );
         stringInstance.setStringValue("asd");
         System.out.println(stringInstance.getStringValue());
-
     }
 
     private static class OneInvocationHandler<T> implements InvocationHandler {
         final private T instance;
 
+        public T getInstance() {
+            return instance;
+        }
+
         public OneInvocationHandler(final T instance) {
             this.instance = instance;
         }
 
-
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (method.getName().startsWith("set") && method.getParameterTypes()[0].equals(String.class)) {
+            if (method.getName().startsWith("set") && method.getParameterTypes().length == 1 && String.class.equals(method.getParameterTypes()[0])) {
                 args[0] = args[0].toString().toUpperCase();
             }
             return method.invoke(instance, args);
