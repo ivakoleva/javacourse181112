36d9d6fb5fde6bf91fe4d5ec01619346dd909e67
dgluharov <dgluharov@gmail.com>
2019-01-29 18:52:42 +0200

    exercise

--- src/main/java/com/musala/javacourse181112/tasks/AnnotationUsageExercise.java
+++ src/main/java/com/musala/javacourse181112/tasks/AnnotationUsageExercise.java
@@ -2,6 +2,8 @@ package com.musala.javacourse181112.tasks;
 
 import com.musala.javacourse181112.annotations.SampleAnnotation;
 
+import java.util.Arrays;
+
 /**
  * Created by Iva Koleva on 29.01.2019
  */
@@ -41,5 +43,17 @@ public class AnnotationUsageExercise {
     @SampleAnnotation
     public static void main(final String[] args) {
         // TODO: implement
+        /*Annotation usage exercise - using template class tasks.AnnotationUsageExercise
+         *** print to stdout all annotated class members*/
+        // loop over all declared fields, filtering them by annotations array not empty
+        // loop over all declared methods, filtering them by annotations array not empty
+
+        final Class<AnnotationUsageExercise> annotationUsageExerciseClass = AnnotationUsageExercise.class;
+        Arrays.stream(annotationUsageExerciseClass.getDeclaredMethods())
+                .filter(method ->)
+
+        //annotationUsageExerciseClass.getDeclaredFields()[0]
+        //annotationUsageExerciseClass.getMethods()[0],annotation;
+
     }
 }
