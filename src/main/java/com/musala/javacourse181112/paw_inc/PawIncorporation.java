0ea32313c8f9dd7c4f1dbf86e775f9c43fbf790a
IcouldEsmileof <aykut@mail.bg>
2019-02-07 11:42:05 +0200

    Project Paw Inc.- with bonus task

--- src/main/java/com/musala/javacourse181112/paw_inc/PawIncorporation.java
+++ src/main/java/com/musala/javacourse181112/paw_inc/PawIncorporation.java
@@ -2,6 +2,8 @@ package com.musala.javacourse181112.paw_inc;
 
 import com.musala.javacourse181112.paw_inc.classes.Command;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -12,15 +14,20 @@ public class PawIncorporation {
     public static void main(final String[] args) {
         final Scanner scan = new Scanner(System.in);
         final AnimalCenterManager animalCenterManager = new AnimalCenterManager();
+        final Map<String, Command> commands = new HashMap<>();
+        for (Command command : Command.values()) {
+            commands.put(command.getLabel(), command);
+        }
         String line;
 
         while (true) {
             line = scan.nextLine();
-            final String[] commands = line.split("[ ][|][ ]");
-            for (Command command : Command.values()) {
-                if (command.getLabel().equals(commands[0])) {
-                    command.invoke(animalCenterManager, commands);
-                }
+            final String[] command = line.split("[ ][|][ ]");
+            if (commands.containsKey(command[0])) {
+                commands.get(command[0]).invoke(animalCenterManager, command);
+            } else {
+                System.err.println("Invalid command");
+                System.exit(1);
             }
         }
     }
